52c08cbf55808dd64723211650aad3c29bacbca6
Ignacio Heredia <ignacio.nh@gmail.com>
2019-02-23 22:37:50 -0300

    Fix chart selector for monitor section

--- html/chart-selector.js
+++ html/chart-selector.js
@@ -1,45 +1,50 @@
-function _bindSelector() {
-    var selector = document.querySelector('.monitor-chart-selector');
-    if (!selector) {
-        setTimeout(_bindSelector, 200);
-    } else {
-        selector.addEventListener('change', _showChartForOption);
+(function () {
+    function _bindSelector(selector, callback) {
+        var element = document.querySelector(selector);
+        if (element && element.getAttribute('data-callback') !== 'set') {
+            element.addEventListener('click', callback);
+            element.setAttribute('data-callback', 'set');
+        }
+        setTimeout(function () {
+            _bindSelector(selector, callback)
+        }, 1000);
     }
-}
-_bindSelector();
 
-function _updateChartSelector(options) {
-    options = options.concat([]);
-    options.sort(function(a, b) {
-        var nameA = (a.programa_desc || a.jurisdiccion_desc).toLowerCase();
-        var nameB = (b.programa_desc || b.jurisdiccion_desc).toLowerCase();
-        if (nameA < nameB)
-            return -1;
-        if (nameA > nameB)
-            return 1;
-        return 0;
-    });
-    var selector = document.querySelector('.monitor-chart-selector');
-    while (selector.firstChild) {
-        selector.removeChild(selector.firstChild);
-    }
-    selector.options[selector.options.length] = new Option('', '');
-    for (var i=0; i<options.length; i++) {
-        var name = options[i].programa_desc || options[i].jurisdiccion_desc;
-        var optionElement = new Option(name, name);
-        optionElement.setAttribute('data-jurisdiction', options[i].jurisdiccion_desc);
-        optionElement.setAttribute('data-program', name);
-        optionElement.setAttribute('data-id', options[i].programa_id);
-        selector.options[selector.options.length] = optionElement;
-    }
-}
+    _bindSelector('.monitor-chart-selector', _showChartForOption);
 
-function _showChartForOption() {
-    var selector = document.querySelector('.monitor-chart-selector');
-    var selectedOption = selector.options[selector.selectedIndex];
-    var jurisdiction = selectedOption.getAttribute('data-jurisdiction');
-    var program = selectedOption.getAttribute('data-program');
-    var id = selectedOption.getAttribute('data-id') ? parseInt(selectedOption.getAttribute('data-id')) : undefined;
-    console.log(program, jurisdiction, id);
-    _loadChartData(program, jurisdiction, id);
-}
+    window._updateChartSelector = function (options) {
+        options = options.concat([]);
+        options.sort(function (a, b) {
+            var nameA = (a.programa_desc || a.jurisdiccion_desc).toLowerCase();
+            var nameB = (b.programa_desc || b.jurisdiccion_desc).toLowerCase();
+            if (nameA < nameB)
+                return -1;
+            if (nameA > nameB)
+                return 1;
+            return 0;
+        });
+        var selector = document.querySelector('.monitor-chart-selector');
+        while (selector.firstChild) {
+            selector.removeChild(selector.firstChild);
+        }
+        selector.options[selector.options.length] = new Option('', '');
+        for (var i = 0; i < options.length; i++) {
+            var name = options[i].programa_desc || options[i].jurisdiccion_desc;
+            var optionElement = new Option(name, name);
+            optionElement.setAttribute('data-jurisdiction', options[i].jurisdiccion_desc);
+            optionElement.setAttribute('data-program', name);
+            optionElement.setAttribute('data-id', options[i].programa_id);
+            selector.options[selector.options.length] = optionElement;
+        }
+    };
+
+    function _showChartForOption() {
+        var selector = document.querySelector('.monitor-chart-selector');
+        var selectedOption = selector.options[selector.selectedIndex];
+        var jurisdiction = selectedOption.getAttribute('data-jurisdiction');
+        var program = selectedOption.getAttribute('data-program');
+        var id = selectedOption.getAttribute('data-id') ? parseInt(selectedOption.getAttribute('data-id')) : undefined;
+        console.log(program, jurisdiction, id);
+        _loadChartData(program, jurisdiction, id);
+    }
+})();
