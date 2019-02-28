2f89fd5b627fa8f417a5b1622c2c8b1d5b7e9964
Ignacio Heredia <ignacio.nh@gmail.com>
2019-02-27 23:47:41 -0300

    Update the update_server script

--- update_server.sh
+++ update_server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 git pull
+
 rm -rf /var/www/html
 cp -R html/ /var/www/html
 systemctl restart nginx
 
-# TODO: update backend
+rm -rf ~/monitor
+cp -R monitor ~/monitor
+systemctl restart monitor
