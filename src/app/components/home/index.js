b17b32913ba7fea2f6d0c22900d2a90011ce9eae
Ignacio Heredia <ignacio.nh@gmail.com>
2019-05-26 23:53:17 -0300

    Add home image

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -4,7 +4,10 @@ require('./index.scss');
 
 const App = () => (
     <Page>
-        <h1>Home2</h1>
+        <a href="/monitor">
+            <img id="background-768" src="/static/portada-768.jpg" alt="Monitor presupuestario" />
+            <div id="background-1920" />
+        </a>
     </Page>
 );
 
