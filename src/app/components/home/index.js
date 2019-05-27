7b64f27d920a0466d5fbe0c88392590d9883f9ab
Ignacio Heredia <ignacio.nh@gmail.com>
2019-05-27 00:32:28 -0300

    Add footer

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -4,7 +4,7 @@ require('./index.scss');
 
 const App = () => (
     <Page>
-        <a href="/monitor">
+        <a href="/monitor" id="home-link">
             <img id="background-768" src="/static/portada-768.jpg" alt="Monitor presupuestario" />
             <div id="background-1920" />
         </a>
--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,12 +1,14 @@
 const React = require('react');
 const Header = require('./components/header');
+const Footer = require('./components/footer');
 require('./index.scss');
 
 const Page = ({ children }) => ([
     <Header />,
     <div id="main">
         { children }
-    </div>
+    </div>,
+    <Footer />
 ]);
 
 module.exports = Page;
