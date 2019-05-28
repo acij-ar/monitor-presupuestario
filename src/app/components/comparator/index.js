ecbe730b8f8f27d9cc547bcfadd1061a68fbc14a
Ignacio Heredia <ignacio.nh@gmail.com>
2019-05-27 22:53:40 -0300

    Add about page with all its texts

--- src/app/components/comparator/index.js
+++ src/app/components/comparator/index.js
@@ -4,7 +4,20 @@ require('./index.scss');
 
 const App = () => (
     <Page>
-        <h1>Comparador</h1>
+        <div className="monitor-highlight">
+            <div className="monitor-content">
+                <h1>Comparador</h1>
+                <p>
+                    En esta sección podés comparar las asignaciones presupuestarias nacionales por jurisdicción,
+                    programa o actividad desde el año 2007 en relación con otras jurisdicciones, programas o
+                    actividades. Podés seleccionar el presupuesto distribuido por el PEN, el presupuesto vigente y el
+                    presupuesto devengado, ajustados por inflación.
+                </p>
+            </div>
+        </div>
+
+        <div className="monitor-content monitor-comparator">
+        </div>
     </Page>
 );
 
