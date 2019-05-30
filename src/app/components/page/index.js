bd7fa0f39834de5b49d7564360a3c3fdbe6d8d1b
Ignacio Heredia <ignacio.nh@gmail.com>
2019-05-29 21:42:11 -0300

    Fix key warning

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -4,11 +4,11 @@ const Footer = require('./components/footer');
 require('./index.scss');
 
 const Page = ({ children }) => ([
-    <Header />,
-    <div id="main">
+    <Header key="header" />,
+    <div id="main" key="main" >
         { children }
     </div>,
-    <Footer />
+    <Footer key="footer" />
 ]);
 
 module.exports = Page;
