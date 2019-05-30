ad80ed474912826c23ff9438501829a76e766fb0
Ignacio Heredia <ignacio.nh@gmail.com>
2019-05-29 22:33:53 -0300

    Add util for available years

--- src/app/controllers/treemap.js
+++ src/app/controllers/treemap.js
@@ -1,22 +1,7 @@
 const axios = require('axios');
+const years = require('../helpers/available-years');
 
 const treemap = (req, res, next) => {
-    const years = [
-        '2007',
-        '2008',
-        '2009',
-        '2010',
-        '2011',
-        '2012',
-        '2013',
-        '2014',
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019',
-    ];
-
     const baseUrl = 'http://monitorpresupuestario.acij.org.ar/api/v1/jurisdiccion?anio=';
     const promises = years.map(year => axios.get(`${baseUrl}${year}`));
     Promise.all(promises)
