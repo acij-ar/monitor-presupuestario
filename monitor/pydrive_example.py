c38354256b4da4410e44be82a060ade4bf50db41
Ignacio Heredia <ignacio.nh@gmail.com>
2019-06-23 23:26:42 -0300

    Update pydrive authentication settings

--- monitor/pydrive_example.py
+++ monitor/pydrive_example.py
@@ -2,8 +2,22 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 # https://pythonhosted.org/PyDrive/oauth.html
+# https://stackoverflow.com/questions/24419188/automating-pydrive-verification-process
+
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("credentials.json")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("credentials.json")
 
 drive = GoogleDrive(gauth)
 
