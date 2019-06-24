256dad2362f160d524164306a7cc97d964249465
Ignacio Heredia <ignacio.nh@gmail.com>
2019-06-23 23:13:25 -0300

    Add reference links

--- monitor/pydrive_example.py
+++ monitor/pydrive_example.py
@@ -1,11 +1,13 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+# https://pythonhosted.org/PyDrive/oauth.html
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
 
 drive = GoogleDrive(gauth)
 
+# https://pythonhosted.org/PyDrive/filemanagement.html#download-file-content
 example_file = drive.CreateFile({'id': '0ByV8mszxSZjFdC0wTjhJam9EWVU'})
 content = example_file.GetContentString()
 print(content)
