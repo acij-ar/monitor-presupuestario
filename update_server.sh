ac8da95942cf6e1837756dd4c027d0d28f7ae393
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-16 23:03:14 -0300

    Add TODO

--- update_server.sh
+++ update_server.sh
@@ -10,4 +10,5 @@ rm -rf ~/monitor
 cp -R monitor ~/monitor
 cp ~/monitor/backend/config.example.py ~/monitor/backend/config.py
 chown -R www-data:root ~/monitor
+# pip3 install -r ~/monitor/requirements.txt # TODO: validate if pip, pip3, conda or virtualenv
 systemctl restart monitor
