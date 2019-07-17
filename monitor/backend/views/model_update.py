22e08e910be70746e279bbd6ab78e74912769a5c
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-16 23:04:41 -0300

    Add request triggers for dataset updates

--- monitor/backend/views/model_update.py
+++ monitor/backend/views/model_update.py
@@ -1,9 +1,11 @@
 from flask_restful import Resource
+from backend.drive import DriveClient
 from backend.model import dataset
 
 
 class DatasetUpdater(Resource):
-    def get(self):
-        dataset.df = dataset.load_from_drive()
-
+    def get(self, year):
+        drive_client = DriveClient()
+        drive_client.download_file_by_year(year, './data/' + year + '.csv')
+        dataset.update_dataset()
         return {"result": "success"}
