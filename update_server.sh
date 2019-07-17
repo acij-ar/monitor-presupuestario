5d18c05c65b0c9103c08d113bec4a803f5e44f34
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-16 23:47:13 -0300

    Update update_server script

--- update_server.sh
+++ update_server.sh
@@ -10,5 +10,6 @@ rm -rf ~/monitor
 cp -R monitor ~/monitor
 cp ~/monitor/backend/config.example.py ~/monitor/backend/config.py
 chown -R www-data:root ~/monitor
-# pip3 install -r ~/monitor/requirements.txt # TODO: validate if pip, pip3, conda or virtualenv
+/root/miniconda3/bin/activate
+pip install -r ~/monitor/requirements.txt
 systemctl restart monitor
