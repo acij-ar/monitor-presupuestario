b899be64336891052140efeac86e5fe804299551
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-19 10:53:42 -0300

    Add tremap-data file

--- src/app/controllers/treemap.js
+++ src/app/controllers/treemap.js
@@ -1,21 +1,10 @@
-const axios = require('axios');
-const years = require('../helpers/available-years');
+const data = require('./treemap-data.json');
 
 const treemap = (req, res, next) => {
-    const baseUrl = 'http://monitorpresupuestario.acij.org.ar/api/v1/jurisdiccion?anio=';
-    const promises = years.map(year => axios.get(`${baseUrl}${year}`));
-    Promise.all(promises)
-        .then((responses) => {
-            const data = {};
-            years.map((year, index) => {
-                data[year] = responses[index].data.map(({ jurisdiccion_desc: name, ...data }) => ({ name, ...data }));
-            });
-            res.locals.props = {
-                treemapData: data
-            };
-            next();
-        })
-        .catch((err) => next(err));
+  res.locals.props = {
+    treemapData: data
+  };
+  next();
 };
 
 module.exports = treemap;
