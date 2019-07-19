3405ea9ae2867d101422b9e0e4d95b9957a481a0
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-19 20:04:43 -0300

    Update nginx confg and update bash script

--- devops/update_server.sh
+++ devops/update_server.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 git pull
+npm i
+npm run dist
+pm2 restart server
 
-rm -rf /var/www/html
-cp -R html/ /var/www/html
+sudo cp nginx.conf /etc/nginx/sites-enabled/monitor
 systemctl restart nginx
-
-rm -rf ~/monitor
-cp -R ~/monitor-presupuestario/monitor ~/monitor
-cp ~/monitor/backend/config.example.py ~/monitor/backend/config.py
-chown -R www-data:root ~/monitor
-/root/miniconda3/bin/activate
-pip install -r ~/monitor/requirements.txt
-systemctl restart monitor
