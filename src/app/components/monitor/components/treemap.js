f702a7ae2e9a56aa842d223a4a184e7cf77faff0
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-19 00:29:40 -0300

    Add selector for presupuesto

--- src/app/components/monitor/components/treemap.js
+++ src/app/components/monitor/components/treemap.js
@@ -1,34 +1,43 @@
 const React = require('react');
 const { Treemap: RechartsTreemap, ResponsiveContainer } = require('recharts');
 const _ = require('lodash');
-const years = require('../../../helpers/available-years');
+const availableYears = require('../../../helpers/available-years');
+const availableKeys = require('../../../helpers/available-keys');
 const downloadChart = require('../../../helpers/download-chart');
 const downloadData = require('../../../helpers/download-csv');
 
 class Treemap extends React.Component {
     constructor(props) {
         super(props);
-        const defaultYear = years[years.length - 1];
         this.state = {
-            selectedYear: defaultYear,
-            dataKey: 'credito_presupuestado',
+            selectedYear: availableYears[availableYears.length - 1],
+            selectedKey: availableKeys[0].key,
         };
-        this.state.data = this.dataForYear(defaultYear);
-        this.onChange = this.onChange.bind(this);
+        this.state.data = this.dataForState();
+
+        this.onYearChange = this.onYearChange.bind(this);
+        this.onKeyChange = this.onKeyChange.bind(this);
         this.downloadData = this.downloadData.bind(this);
     }
 
-    dataForYear(year) {
-        const dataForYear = this.props.data[year];
-        return _.sortBy(dataForYear, dataPoint => -dataPoint[this.state.dataKey]);
+    dataForState() {
+        const dataForYear = this.props.data[this.state.selectedYear];
+        return _.sortBy(dataForYear, dataPoint => -dataPoint[this.state.selectedKey]);
     }
 
-    onChange(e) {
-        const selectedYear = e.target.value;
-        this.setState({
-            selectedYear,
-            data: this.dataForYear(selectedYear),
-        });
+    setDataForState() {
+        this.state.data = this.dataForState();
+        this.setState(this.state);
+    }
+
+    onYearChange(e) {
+        this.state.selectedYear = e.target.value;
+        this.setDataForState();
+    }
+
+    onKeyChange(e) {
+        this.state.selectedKey = e.target.value;
+        this.setDataForState();
     }
 
     static downloadImage() {
@@ -38,7 +47,7 @@ class Treemap extends React.Component {
     downloadData() {
         const rows = [
             ["nombre", "valor"],
-            ...this.state.data.map(dataPoint => [dataPoint.name, dataPoint[this.state.dataKey]])
+            ...this.state.data.map(dataPoint => [dataPoint.name, dataPoint[this.state.selectedKey]])
         ];
         downloadData(rows, 'treemap.csv');
     }
@@ -47,20 +56,25 @@ class Treemap extends React.Component {
 
         return (
             <React.Fragment>
-                <select onChange={this.onChange} defaultValue={this.state.selectedYear}>
-                    {
-                        years.map(year => (
-                            <option key={year} defaultValue={year}>
-                                { year }
-                            </option>
-                        ))
-                    }
+                <select onChange={this.onYearChange} defaultValue={this.state.selectedYear}>
+                    { availableYears.map(year => (
+                        <option key={year} defaultValue={year}>
+                            { year }
+                        </option>
+                    ))}
+                </select>
+                <select onChange={this.onKeyChange} defaultValue={this.state.selectedKey}>
+                    { availableKeys.map(({ key, name }) => (
+                        <option key={key} value={key}>
+                            { name }
+                        </option>
+                    ))}
                 </select>
                 <ResponsiveContainer width="100%" aspect={3}>
                     {/* TODO: add placeholder for client first load */}
                     <RechartsTreemap
                         data={this.state.data}
-                        dataKey={this.state.dataKey}
+                        dataKey={this.state.selectedKey}
                         ratio={4 / 3}
                         stroke="#fff"
                         fill="#8884d8"
