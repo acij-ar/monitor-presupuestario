9b848307040cb2c7710f236d257a7114cf7d9946
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-19 18:12:36 -0300

    Add sqlite test file

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const DataClient = require('../data');
 
-router.get('/', (req, res) => {
-    res.send('ping');
+router.get('/ping', (req, res) => {
+    res.send('pong');
 });
 
 module.exports = router;
---
+++ src/data/index.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, '..', 'data', 'db.sqlite3');
+let db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the sqlite db');
+});
+
+class DataClient {
+
+}
+
+module.exports = DataClient;
