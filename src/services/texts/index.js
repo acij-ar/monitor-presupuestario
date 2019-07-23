5d8e4bbb1951ac1d48f03f9509682ec2bb58cdfd
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-23 16:46:15 -0300

    Simplify texts json structure

--- src/services/texts/index.js
+++ src/services/texts/index.js
@@ -4,30 +4,12 @@ const path = require('path');
 class Texts {
     constructor() {
         this.filePath = path.join(__dirname, 'texts.json');
-        this.rawFile = JSON.parse(fs.readFileSync(this.filePath));
-        this.parseContent();
-    }
-
-    parseContent() {
-        const monitorSection = this.rawFile.find(section => section.id === 'monitor');
-        const comparatorSection = this.rawFile.find(section => section.id === 'comparator');
-        const getText = (section, textId) => section.texts.find(text => text.id === textId).content;
-        this.content = {
-            monitor: {
-                title: getText(monitorSection, 'title'),
-                description: getText(monitorSection, 'description'),
-            },
-            comparator: {
-                title: getText(comparatorSection, 'title'),
-                description: getText(comparatorSection, 'description'),
-            }
-        }
+        this.content = JSON.parse(fs.readFileSync(this.filePath));
     }
 
     saveNewContent(newContent) {
-        this.rawFile = newContent;
+        this.content = newContent;
         fs.writeFileSync(this.filePath, JSON.stringify(newContent, null, 2));
-        this.parseContent();
     }
 }
 
