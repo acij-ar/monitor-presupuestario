45f88d82e8dc692b45900a4e61a89af9e21cfd74
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-23 16:46:46 -0300

    Add api call to save new texts

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const DataClient = require('../services/data');
+const Texts = require('../services/texts');
+
+router.use(express.json());
 
 router.get('/ping', (req, res) => {
     res.send('pong');
+});
+
+router.post('/admin/texts', (req, res) => {
+    Texts.saveNewContent(req.body.texts);
+    res.json({ success: true });
 });
 
 module.exports = router;
--- src/app/pages/admin/components/texts-form.js
+++ src/app/pages/admin/components/texts-form.js
@@ -1,8 +1,33 @@
 const React = require('react');
+const axios = require('axios');
 
 class TextsForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            texts: props.texts,
+        };
+        this.saveTexts = this.saveTexts.bind(this);
+    }
+
+    onTextChange() {
+        this.state.saveError = false;
+        this.state.saveSuccessfull = false;
+        this.setState(this.state);
+    }
+
+    saveTexts() {
+        this.onTextChange();
+        const texts = this.state.texts;
+        axios.post('/api/admin/texts', { texts })
+            .then(() => {
+                this.state.saveSuccessfull = true;
+                this.setState(this.state);
+            })
+            .catch(() => {
+                this.state.saveError = true;
+                this.setState(this.state);
+            })
     }
 
     render() {
@@ -12,18 +37,33 @@ class TextsForm extends React.Component {
                 <div className="monitor-admin-page-section">
                     <h2>Monitor</h2>
                     <h3>Título</h3>
-                    <input defaultValue={texts.monitor.title}/>
+                    <input value={texts.monitor.title} onChange={e => {
+                        this.state.texts.monitor.title = e.target.value;
+                        this.onTextChange();
+                    }}/>
                     <h3>Descripción</h3>
-                    <textarea>{texts.monitor.description}</textarea>
+                    <textarea value={texts.monitor.description} onChange={e => {
+                        this.state.texts.monitor.description = e.target.value;
+                        this.onTextChange();
+                    }}/>
                 </div>
 
                 <div className="monitor-admin-page-section">
                     <h2>Comparador</h2>
                     <h3>Título</h3>
-                    <input value={texts.comparator.title}/>
+                    <input value={texts.comparator.title} onChange={e => {
+                        this.state.texts.comparator.title = e.target.value;
+                        this.onTextChange();
+                    }}/>
                     <h3>Descripción</h3>
-                    <textarea>{texts.comparator.description}</textarea>
+                    <textarea value={texts.comparator.description} onChange={e => {
+                        this.state.texts.comparator.description = e.target.value;
+                        this.onTextChange();
+                    }}/>
                 </div>
+                <button onClick={this.saveTexts}>Guardar</button>
+                { this.state.saveSuccessfull && <span>✅</span>}
+                { this.state.saveError && <span>❌</span>}
             </div>
         )
     }
