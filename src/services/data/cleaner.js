1355b40e0400e2d86239c89ac4bd4284c6f350a5
Ignacio Heredia <ignacio.nh@gmail.com>
2019-07-26 15:48:53 -0300

    Parsing 1e6 floats

--- src/services/data/cleaner.js
+++ src/services/data/cleaner.js
@@ -1,33 +1,29 @@
 const csv = require('fast-csv');
 const fs = require('fs');
 const CSVWriteStream = require('csv-write-stream');
+const columns = require('../dataset-columns');
+
+const parseNumericValue = numericString => {
+    if (numericString.includes(',')) {
+        const [ integerString, decimalString ] = numericString.split(',');
+        const decimalValue = parseFloat(`0.${decimalString}`) * 1e6;
+        return parseInt(integerString) * 1e6 + Math.floor(decimalValue)
+    }
+    return parseInt(numericString) * 1e6;
+};
 
 module.exports = ({rawFilePath, cleanedFilePath}) => {
     console.log(`Cleaning file ${rawFilePath}`);
-    const not_numeric_columns = [
-        'ejercicio_presupuestario',
-        'jurisdiccion_desc',
-        'entidad_id',
-        'entidad_desc',
-        'programa_id',
-        'programa_desc',
-        'actividad_id',
-        'actividad_desc'
-    ];
-    const numeric_columns = [
-        'credito_presupuestado',
-        'credito_vigente',
-        'credito_comprometido',
-        'credito_devengado',
-        'credito_pagado'
-    ];
+    const headers = columns.map(column => column.name);
+    const numeric_columns = columns.filter(column => column.isNumeric).map(column => column.name);
     const outputStream = fs.createWriteStream(cleanedFilePath);
-    const csvWriter = CSVWriteStream({headers: not_numeric_columns.concat(numeric_columns)});
+    const csvWriter = CSVWriteStream({headers});
     csvWriter.pipe(outputStream);
 
     fs.createReadStream(rawFilePath)
         .pipe(csv.parse({headers: true}))
         .on('data', row => {
+            numeric_columns.map(columnName => row[columnName] = parseNumericValue(row[columnName]));
             csvWriter.write(row);
         })
         .on('end', () => {
