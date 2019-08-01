6ce21dda847e53928f8d84ad2229112e258250db
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-01 19:55:36 -0300

    Simple inserts for inflacion table

--- src/services/dataset-updater/index.js
+++ src/services/dataset-updater/index.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const availableDatasets = require('../../config').datasets.files;
 const googleDriveClient = require('./google-drive-client');
 const datasetCleaner = require('./cleaner');
-const csv2json = require('./csv2json');
-const datasetQueries = require('../dataset-queries');
+const dbUpdater = require('./db-updater');
 
 class DatasetUpdater {
     constructor() {
@@ -17,16 +16,19 @@ class DatasetUpdater {
         this.processing = true;
         const dataset = availableDatasets.find(file => file.filename === datasetFilename);
         const {id: fileId, rawPath, filePath} = dataset;
+
         console.log(`Downloading ${fileId} to ${rawPath}`);
         await googleDriveClient.downloadFile({fileId, outputPath: rawPath});
+
         if (datasetFilename === 'inflacion.csv') {
             console.log('Skipping cleaning routine');
             fs.copyFileSync(rawPath, filePath)
         } else {
             await datasetCleaner(dataset);
         }
-        await csv2json();
-        await datasetQueries.update();
+
+        await dbUpdater();
+
         this.processing = false;
     }
 }
