214c13c3ea24f372250ab4831244181c9a0c5914
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-01 21:35:10 -0300

    Add drop tables method

--- src/services/db/index.js
+++ src/services/db/index.js
@@ -1,6 +1,7 @@
 const sqlite3 = require('sqlite3');
-const { path } = require('../../config').db;
+const {path} = require('../../config').db;
 const createTablesIfNotExistQueries = require('./queries/create-tables');
+const dropTableQueries = require('./queries/drop-tables');
 
 class DataBase {
     constructor() {
@@ -10,8 +11,18 @@ class DataBase {
                 throw err
             }
         });
+        this.createTablesIfNotExist();
+    }
+
+    createTablesIfNotExist() {
         this.sqlite.serialize(() => {
             createTablesIfNotExistQueries.map(query => this.sqlite.run(query));
+        });
+    }
+
+    dropTables() {
+        this.sqlite.serialize(() => {
+            dropTableQueries.map(query => this.sqlite.run(query));
         });
     }
 }
---
+++ src/services/db/queries/drop-tables.js
@@ -0,0 +1,7 @@
+module.exports = [
+    'DROP TABLE IF EXISTS jurisdicciones',
+    'DROP TABLE IF EXISTS entidades',
+    'DROP TABLE IF EXISTS actividades',
+    'DROP TABLE IF EXISTS programas',
+    'DROP TABLE IF EXISTS inflacion',
+];
