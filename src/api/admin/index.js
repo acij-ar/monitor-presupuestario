98e28fee231f896d9a9f215044c76341ab7288b4
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-03 23:47:34 -0300

    Minor ordering

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -6,11 +6,8 @@ const updateTextsController = require('./controllers/update-texts');
 const {updateDataset, jobStatus} = require('./controllers/update-datasets');
 
 router.post('/texts', requireLogin, updateTextsController);
-
 router.post('/update_dataset/:filename', requireLogin, updateDataset);
-
 router.get('/dataset_job_status', requireLogin, jobStatus);
-
 router.post('/login', loginController);
 
 module.exports = router;
