ac613dce8599c76af67ea02b457d3f557386c324
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-04 21:57:35 -0300

    Extract db inserts into function

--- src/services/dataset-updater/db-updater/update-budget-tables.js
+++ src/services/dataset-updater/db-updater/update-budget-tables.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
 const db = require('../../db');
-const { jsonPath } = require('../../../config').db;
+const {jsonPath} = require('../../../config').db;
+
+const insertInto = ({tableName, keys, object}) => {
+    const columns = keys.join(', ');
+    const values = keys.map(key => object[key]);
+    const placeholders = values.map(() => '?').join(', ');
+    return db.sqlite.run(`INSERT INTO ${tableName}(${columns}) VALUES(${placeholders})`, values)
+};
+
+const baseKeys = [
+    'name', 'year', 'credito_presupuestado', 'credito_vigente',
+    'credito_comprometido', 'credito_devengado', 'credito_pagado'
+];
 
 const updateJurisdiction = async (jurisdiccion) => {
-    const {
-        name, year, credito_presupuestado, credito_vigente,
-        credito_comprometido, credito_devengado, credito_pagado
-    } = jurisdiccion;
-    const { lastID } = await db.sqlite.run(
-        `INSERT INTO jurisdicciones(
-            year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado
-        ) VALUES(?, ?, ?, ?, ?, ?, ?)`,
-        [year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado]
-    );
+    const {lastID} = await insertInto({tableName: 'jurisdicciones', keys: baseKeys, object: jurisdiccion});
     Object.keys(jurisdiccion.dependencies).map(entidad => {
         updateEntity({
-            year,
+            year: jurisdiccion.year,
             name: entidad,
             jurisdiccion_id: lastID,
             ...jurisdiccion.dependencies[entidad]
@@ -24,21 +27,13 @@ const updateJurisdiction = async (jurisdiccion) => {
 };
 
 const updateEntity = async (entity) => {
-    const {
-        name, year, credito_presupuestado, credito_vigente,
-        credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id
-    } = entity;
-    const { lastID } = await db.sqlite.run(
-        `INSERT INTO entidades(
-            year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id
-        ) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
-        [year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id]
-    );
+    const keys = ['jurisdiccion_id', ...baseKeys];
+    const {lastID} = await insertInto({tableName: 'entidades', keys, object: entity});
     Object.keys(entity.dependencies).map(program => {
         updateProgram({
-            year,
+            year: entity.year,
             name: program,
-            jurisdiccion_id,
+            jurisdiccion_id: entity.jurisdiccion_id,
             entidad_id: lastID,
             ...entity.dependencies[program]
         })
@@ -46,22 +41,14 @@ const updateEntity = async (entity) => {
 };
 
 const updateProgram = async (program) => {
-    const {
-        name, year, credito_presupuestado, credito_vigente,
-        credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id
-    } = program;
-    const { lastID } = await db.sqlite.run(
-        `INSERT INTO programas(
-            year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id
-        ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id]
-    );
+    const keys = ['jurisdiccion_id', 'entidad_id', ...baseKeys];
+    const {lastID} = await insertInto({tableName: 'programas', keys, object: program});
     Object.keys(program.dependencies).map(activity => {
         updateActivity({
-            year,
+            year: program.year,
             name: activity,
-            jurisdiccion_id,
-            entidad_id,
+            jurisdiccion_id: program.jurisdiccion_id,
+            entidad_id: program.entidad_id,
             programa_id: lastID,
             ...program.dependencies[activity]
         })
@@ -69,19 +56,12 @@ const updateProgram = async (program) => {
 };
 
 const updateActivity = (activity) => {
-    const {
-        name, year, credito_presupuestado, credito_vigente,
-        credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id, programa_id
-    } = activity;
-    db.sqlite.run(
-        `INSERT INTO actividades(
-            year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id, programa_id
-        ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [year, name, credito_presupuestado, credito_vigente, credito_comprometido, credito_devengado, credito_pagado, jurisdiccion_id, entidad_id, programa_id]
-    );
+    const keys = ['jurisdiccion_id', 'entidad_id', 'programa_id', ...baseKeys];
+    insertInto({tableName: 'actividades', keys, object: activity});
 };
 
-module.exports = () => {
+module.exports = async() => {
+    await db.initPromise;
     const jsonContent = fs.readFileSync(jsonPath);
     const jsonDB = JSON.parse(jsonContent);
     Object.keys(jsonDB).map(year => {
