5094f17895f5b1cfd5995b0d8d81121756a394a2
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-05 14:24:12 -0300

    Add treemap api response

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SearchService = require('../../services/search');
 const searchList = require('../../services/db/queries/list-for-select');
+const treemap = require('../../services/db/queries/treemap');
 
 router.get('/search', async (req, res) => {
     const searchString = req.query.q;
@@ -13,6 +14,12 @@ router.get('/search-list', async (req, res) => {
     const {table} = req.query;
     const results = await searchList({table});
     res.json(results)
+});
+
+router.get('/treemap', async (req, res) => {
+    const {parentTable, parentName, year, budgetType} = req.query;
+    const treemapData = await treemap({parentTable, parentName, year, budgetType});
+    res.json(treemapData)
 });
 
 module.exports = router;
--- src/services/db/queries/treemap.js
+++ src/services/db/queries/treemap.js
@@ -1,5 +1,28 @@
 const db = require('..');
+const _ = require('lodash');
 
-module.exports = async ({entityType, name, year, budgetType}) => {
-    const {id} = await db.sqlite.get(`SELECT id FROM ${entityType} WHERE year = ? AND name = ?`, [year, name]);
+module.exports = async ({parentTable, parentName, year, budgetType}) => {
+    const {id: parentId} = await db.sqlite.get(
+        `SELECT id FROM ${parentTable} WHERE year = ? AND name = ?`,
+        [year, parentName],
+    );
+    const title = `${parentName} (${budgetType} ${year})`;
+    if (!parentId) {
+        return {title};
+    }
+    const childTable = parentTable === 'jurisdicciones' ? 'programas' : 'actividades';
+    const pkColumn = parentTable === 'jurisdicciones' ? 'jurisdiccion_id' : 'programa_id';
+    const results = await db.sqlite.all(
+        `SELECT name, id, ${budgetType} FROM ${childTable} WHERE year = ? AND ${pkColumn} = ? ORDER BY ${budgetType} DESC`,
+        [year, parentId]
+    );
+
+    return {
+        data: [
+            ['name', 'parent', 'value'],
+            ['root', null, _.sumBy(results, budgetType)],
+            ...results.map(result => [result.name, 'root', result[budgetType]])
+        ],
+        title,
+    }
 };
