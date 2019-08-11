0a74af67e6d86191466dfa7977a743cc38f3c812
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-11 19:04:19 -0300

    Update available budgets options

--- src/app/pages/monitor/helpers/available-budgets.js
+++ src/app/pages/monitor/helpers/available-budgets.js
@@ -1,7 +1,5 @@
 module.exports = [
-    {label: 'Crédito presupuestado', value: 'credito_presupuestado'},
+    {label: 'Crédito original', value: 'credito_original'},
     {label: 'Crédito vigente', value: 'credito_vigente'},
-    {label: 'Crédito comprometido', value: 'credito_comprometido'},
     {label: 'Crédito devengado', value: 'credito_devengado'},
-    {label: 'Crédito pagado', value: 'credito_pagado'},
 ];
