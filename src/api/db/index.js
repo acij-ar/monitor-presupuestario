64ecb0ea74c37b3ac2393d67d97250ae2a83deb8
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-11 18:58:25 -0300

    Mock bar-chart data response from api

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const SearchService = require('../../services/search');
 const searchList = require('../../services/db/queries/list-for-select');
 const treemap = require('../../services/db/queries/treemap');
+const barchart = require('../../services/db/queries/bar-chart');
 
 router.get('/search', async (req, res) => {
     const searchString = req.query.q;
@@ -21,5 +22,12 @@ router.get('/treemap', async (req, res) => {
     const treemapData = await treemap({parentTable, parentName, year, budgetType});
     res.json(treemapData)
 });
+
+router.get('/bar-chart', async (req, res) => {
+    const {selectedYears, selectedBudgets, selectedEntities} = req.query;
+    const barchartData = await barchart({selectedYears, selectedBudgets, selectedEntities});
+    res.json(barchartData)
+});
+
 
 module.exports = router;
