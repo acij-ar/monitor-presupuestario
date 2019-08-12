58705c8788b2ebbe58074aa7aacc22ee3734492b
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-11 23:48:34 -0300

    Showing all available budgets

--- src/app/pages/monitor/helpers/available-budgets.js
+++ src/app/pages/monitor/helpers/available-budgets.js
@@ -1,5 +1,8 @@
 module.exports = [
+    {label: 'Crédito presupuestado', value: 'credito_presupuestado_ajustado'},
     {label: 'Crédito original', value: 'credito_original_ajustado'},
+    {label: 'Crédito comprometido', value: 'credito_comprometido_ajustado'},
     {label: 'Crédito vigente', value: 'credito_vigente_ajustado'},
     {label: 'Crédito devengado', value: 'credito_devengado_ajustado'},
+    {label: 'Crédito pagado', value: 'credito_pagado_ajustado'},
 ];
