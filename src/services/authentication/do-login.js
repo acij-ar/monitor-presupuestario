926ad847d1566489d9df2f761c61c579ec77497c
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-13 18:38:24 -0300

    Fix login maxAge

--- src/services/authentication/do-login.js
+++ src/services/authentication/do-login.js
@@ -3,7 +3,7 @@ const { password } = require('./credentials.json');
 module.exports = (req, res) => {
     const { password: postedPassword } = req.body;
     if (postedPassword === password) {
-        const oneDay = 60 * 60 * 24;
+        const oneDay = 60 * 60 * 24 * 1000;
         res.cookie('password', password, { maxAge: oneDay });
         return true
     }
