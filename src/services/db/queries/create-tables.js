a310e4f196effb08d6704727379361bedf2e4b37
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-19 19:25:39 -0300

    Thinking a new db schema

--- src/services/db/queries/create-tables.js
+++ src/services/db/queries/create-tables.js
@@ -1,119 +1,28 @@
 module.exports = [
-    `
-        CREATE TABLE IF NOT EXISTS años(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            year INTEGER NOT NULL, 
-            credito_presupuestado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado INTEGER DEFAULT 0 NOT NULL,
-            credito_original INTEGER DEFAULT 0 NOT NULL,
-            credito_presupuestado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_posiblemente_modificado BOOLEAN DEFAULT 0 NOT NULL,
-            UNIQUE(year)
-        )
-    `,
-    `
-        CREATE TABLE IF NOT EXISTS jurisdicciones(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            year INTEGER NOT NULL, 
-            name TEXT NOT NULL,
-            credito_presupuestado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado INTEGER DEFAULT 0 NOT NULL,
-            credito_original INTEGER DEFAULT 0 NOT NULL,
-            credito_presupuestado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_posiblemente_modificado BOOLEAN DEFAULT 0 NOT NULL,
-            UNIQUE(year, name)
-        )
-    `,
-    `
-        CREATE TABLE IF NOT EXISTS entidades(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            year INTEGER NOT NULL,
-            jurisdiccion_id INTEGER NOT NULL, 
-            name TEXT NOT NULL,
-            credito_presupuestado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado INTEGER DEFAULT 0 NOT NULL,
-            credito_original INTEGER DEFAULT 0 NOT NULL,
-            credito_presupuestado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_posiblemente_modificado BOOLEAN DEFAULT 0 NOT NULL,
-            CONSTRAINT foreign_key_jurisdiccion FOREIGN KEY (jurisdiccion_id) REFERENCES jurisdicciones (id),
-            UNIQUE(year, jurisdiccion_id, name)
-        )
-    `,
-    `
-        CREATE TABLE IF NOT EXISTS programas(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            year INTEGER NOT NULL,
-            jurisdiccion_id INTEGER NOT NULL,
-            entidad_id INTEGER NOT NULL, 
-            name TEXT NOT NULL,
-            credito_presupuestado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado INTEGER DEFAULT 0 NOT NULL,
-            credito_original INTEGER DEFAULT 0 NOT NULL,
-            credito_presupuestado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_posiblemente_modificado BOOLEAN DEFAULT 0 NOT NULL,
-            CONSTRAINT foreign_key_jurisdiccion FOREIGN KEY (jurisdiccion_id) REFERENCES jurisdicciones (id),
-            CONSTRAINT foreign_key_entidad FOREIGN KEY (entidad_id) REFERENCES entidades (id),
-            UNIQUE(year, jurisdiccion_id, entidad_id, name)
-        )
-    `,
-    `
-        CREATE TABLE IF NOT EXISTS actividades(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            year INTEGER NOT NULL,
-            jurisdiccion_id INTEGER NOT NULL,
-            entidad_id INTEGER NOT NULL, 
-            programa_id INTEGER NOT NULL, 
-            name TEXT NOT NULL,
-            credito_presupuestado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado INTEGER DEFAULT 0 NOT NULL,
-            credito_original INTEGER DEFAULT 0 NOT NULL,
-            credito_presupuestado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_vigente_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_comprometido_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_devengado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_pagado_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_ajustado INTEGER DEFAULT 0 NOT NULL,
-            credito_original_posiblemente_modificado BOOLEAN DEFAULT 0 NOT NULL,
-            CONSTRAINT foreign_key_jurisdiccion FOREIGN KEY (jurisdiccion_id) REFERENCES jurisdicciones (id),
-            CONSTRAINT foreign_key_entidad FOREIGN KEY (entidad_id) REFERENCES entidades (id),
-            CONSTRAINT foreign_key_programa FOREIGN KEY (programa_id) REFERENCES programas (id),
-            UNIQUE(year, jurisdiccion_id, entidad_id, programa_id, name)
-        )
-    `
+    `CREATE TABLE entidades(
+        id              INTEGER PRIMARY KEY AUTOINCREMENT,
+        name            TEXT NOT NULL,
+        entity_type     TEXT NOT NULL
+        parent_id       INTEGER,
+        UNIQUE(name, parent_id)
+    )`,
+    `CREATE TABLE presupuestos(
+        entity_id                                   INTEGER NOT NULL,
+        year                                        INTEGER NOT NULL,
+        credito_presupuestado                       INTEGER NOT NULL,
+        credito_vigente                             INTEGER NOT NULL,
+        credito_comprometido                        INTEGER NOT NULL,
+        credito_devengado                           INTEGER NOT NULL,
+        credito_pagado                              INTEGER NOT NULL,
+        credito_original                            INTEGER NOT NULL,
+        credito_presupuestado_ajustado              INTEGER NOT NULL,
+        credito_vigente_ajustado                    INTEGER NOT NULL,
+        credito_comprometido_ajustado               INTEGER NOT NULL,
+        credito_devengado_ajustado                  INTEGER NOT NULL,
+        credito_pagado_ajustado                     INTEGER NOT NULL,
+        credito_original_ajustado                   INTEGER NOT NULL,
+        credito_original_posiblemente_modificado    BOOLEAN NOT NULL,
+        CONSTRAINT foreign_key_entity FOREIGN KEY (entity_id) REFERENCES entidades (id),
+        UNIQUE(year, entity_id)
+    )`,
 ];
