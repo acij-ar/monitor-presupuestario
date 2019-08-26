b120fbe5d600a4fa02e91b59ed418cdee71c6a96
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-26 00:17:38 -0300

    Rewrite chart queries

--- src/app/pages/monitor/helpers/available-budgets.js
+++ src/app/pages/monitor/helpers/available-budgets.js
@@ -2,36 +2,36 @@ module.exports = [
     /*
     {
         label: 'Crédito presupuestado',
-        value: 'credito_presupuestado_ajustado',
+        value: 'credito_presupuestado',
         color: '#642370'
     },
     */
     {
         label: 'Crédito original',
-        value: 'credito_original_ajustado',
+        value: 'credito_original',
         color: '#e600c8'
     },
     /*
     {
         label: 'Crédito comprometido',
-        value: 'credito_comprometido_ajustado',
+        value: 'credito_comprometido',
         color: '#0a256a'
     },
     */
     {
         label: 'Crédito vigente',
-        value: 'credito_vigente_ajustado',
+        value: 'credito_vigente',
         color: '#0ce6ae'
     },
     {
         label: 'Crédito devengado',
-        value: 'credito_devengado_ajustado',
+        value: 'credito_devengado',
         color: '#0039eb'
     },
     /*
     {
         label: 'Crédito pagado',
-        value: 'credito_pagado_ajustado',
+        value: 'credito_pagado',
         color: '#c42525'
     },
     */
--- src/services/db/charts/treemap/query.js
+++ src/services/db/charts/treemap/query.js
@@ -1,43 +1,16 @@
 const db = require('../..');
 const availableYears = require('../../../../app/pages/monitor/helpers/available-years');
 const availableBudgets = require('../../../../app/pages/monitor/helpers/available-budgets');
-const {db: {tables}} = require('../../../../config');
 
 // TODO: combinations of year and entity may not exists. Handle these cases
 
 module.exports = ({selectedYear, selectedBudget, selectedEntity}) => {
     const year = (selectedYear || availableYears[0]).label;
     const {value: budgetValue, label: budgetName, color} = (selectedBudget || availableBudgets[0]);
-    const entity = (selectedEntity || {table: 'años', name: 'Presupuesto total'});
-    const tableConfig = tables.find(table => table.name === entity.table);
+    const entity = (selectedEntity || {name: 'Presupuesto total', id: 1});
 
-    let data, name;
-    if (entity.table === 'años') {
-        data = db.prepare(
-            `SELECT name, ${budgetValue} AS value FROM jurisdicciones 
-                    WHERE year = ${year} ORDER BY ${budgetValue} DESC`).all();
-        name = `${entity.name} - ${year} ${budgetName}`;
-    } else if (tableConfig.childTable) {
-        // TODO: use ids instead of names for the following query
-        const {parentId} = db.prepare(
-            `SELECT id AS parentId from ${entity.table} WHERE year = ${year} AND name = ?`).get(entity.name);
-        data = db.prepare(
-            `SELECT name, ${budgetValue} AS value FROM ${tableConfig.childTable} 
-                    WHERE ${tableConfig.primaryKeyForChild} = ${parentId} AND year = ${year} 
-                    ORDER BY ${budgetValue} DESC`).all();
-        name = `${entity.name} - ${year} ${budgetName}`;
-    } else {
-        // TODO: use ids instead of names for the following query
-        const {parentId} = db.prepare(
-            `SELECT ${tableConfig.primaryKeyForParent} AS parentId FROM ${entity.table} WHERE year = ${year} AND name = ?`).get(entity.name);
-        const {parentName} = db.prepare(`SELECT name AS parentName FROM ${tableConfig.parentTable} WHERE id = ?`).get(parentId);
-        data = db.prepare(
-            `SELECT name, ${budgetValue} AS value FROM ${entity.table} 
-                    WHERE ${tableConfig.primaryKeyForParent} = ${parentId} AND year = ${year}
-                    ORDER BY ${budgetValue} DESC`).all();
-        name = `${parentName} - ${year} ${budgetName}`;
-    }
+    const data = db.prepare(`SELECT name, ${budgetValue} AS value FROM entidades JOIN presupuestos ON entidades.id = entity_id WHERE entidades.parent_id = ? AND year = ?`).all(entity.id, year);
+    const name = `${entity.name} - ${year} ${budgetName}`;
 
     return {name, data, color};
-
 };
