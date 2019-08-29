59c871283904971d7359ad1a2b5e43a997ac6e92
Ignacio Heredia <ignacio.nh@gmail.com>
2019-08-29 00:03:06 -0300

    Treemap with drill down

--- src/services/db/charts/treemap/query.js
+++ src/services/db/charts/treemap/query.js
@@ -7,8 +7,28 @@ module.exports = ({selectedYear, selectedBudget, selectedEntity}) => {
     const {value: budgetValue, label: budgetName, color} = (selectedBudget || availableBudgets[0]);
     const entity = (selectedEntity || {name: 'Presupuesto total', id: 1});
 
-    const data = db.prepare(`SELECT name, ${budgetValue} AS value FROM entidades JOIN presupuestos ON entidades.id = entity_id WHERE entidades.parent_id = ? AND year = ?`).all(entity.id, year);
+    const data = db.prepare(`
+        SELECT presupuestos.${budgetValue} AS value, entidades.name AS name, entidades.id AS tableId, entidades.parent_id as parentId 
+        FROM presupuestos 
+        JOIN relaciones ON relaciones.child_id = presupuestos.entity_id
+        JOIN entidades ON presupuestos.entity_id = entidades.id 
+        WHERE relaciones.parent_id = ? AND presupuestos.year = ?
+    `).all(entity.id, year);
     const name = `${entity.name} - ${year} ${budgetName}`;
 
+    const tree = {};
+    const getId = (row) => {
+        const { tableId, parentId } = row;
+        if (tableId === entity.id) {
+            row.id = `id_${entity.id}`;
+        } else {
+            row.id = `${tree[parentId].id}_${tableId}`;
+            row.parent = tree[parentId].id;
+        }
+        delete row.tableId;
+        delete row.parentId;
+        tree[tableId] = row;
+    };
+    data.map(getId);
     return {name, data, color};
 };
