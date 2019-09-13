817b68114f5509b06ebb7f8eaaccdaa99095015b
Ignacio Heredia <ignacio.nh@gmail.com>
2019-09-13 18:08:45 -0300

    Update title and legens for column chart

--- src/services/db/charts/historic-bar-chart/query.js
+++ src/services/db/charts/historic-bar-chart/query.js
@@ -8,18 +8,20 @@ module.exports = ({selectedYears, selectedBudgets, selectedEntities}) => {
   const years = _.sortBy((selectedYears || availableYears).map(year => year.label));
   selectedBudgets = selectedBudgets || availableBudgets;
   const budgetsValues = selectedBudgets.map(budget => budget.value).join(', ');
-  const entities = selectedEntities || [{name: 'Presupuesto total', id: 1}];
+  const entities = selectedEntities || [{label: 'Presupuesto total', id: 1}];
 
   const series = [];
-  entities.map(({name, id, groupId}, index) => {
+  let seriesAreGrouped = false;
+  entities.map(({label, id, groupId}, index) => {
     const results = db.prepare(`SELECT year, ${budgetsValues} FROM entidades JOIN presupuestos ON entidades.id = entity_id WHERE entidades.id = ? ORDER BY year ASC`).all(id);
     selectedBudgets.map(budget => {
       const serie = {
-        name: `${name} - ${budget.label}`,
+        name: `${label} - ${budget.label}`,
         data: [],
         color: entities.length > 1 ? patternFill({pathNumber: index, strokeColor: budget.color}) : budget.color,
         stack: groupId ? `grouped-${budget.value}-${groupId}` : `ungrouped-${budget.value}-${id}`,
       };
+      seriesAreGrouped = seriesAreGrouped || !!groupId;
       const budgetByYear = {};
       results.map(result => budgetByYear[result.year] = result[budget.value]);
       years.map(year => serie.data.push(budgetByYear[year] || 0));
@@ -27,7 +29,7 @@ module.exports = ({selectedYears, selectedBudgets, selectedEntities}) => {
     });
   });
 
-  const titleText = entities.map(({name}) => name).join(', ');
+  const titleText = seriesAreGrouped ? 'Seleccion de programas agrupados segun referencias' : entities.map(({name}) => name).join(', ');
 
   return {
     years,
