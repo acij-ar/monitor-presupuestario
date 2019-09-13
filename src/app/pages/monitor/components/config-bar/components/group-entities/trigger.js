de64c6ac6653fd5160cbd56dfe3ef856b9c6fa69
Ignacio Heredia <ignacio.nh@gmail.com>
2019-09-13 01:10:33 -0300

    Add tooltip to group trigger

--- src/app/pages/monitor/components/config-bar/components/group-entities/trigger.js
+++ src/app/pages/monitor/components/config-bar/components/group-entities/trigger.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const ReactTooltip = require('react-tooltip');
 
 class GroupEntitiesTrigger extends React.PureComponent {
   render() {
@@ -9,7 +10,14 @@ class GroupEntitiesTrigger extends React.PureComponent {
         <label
           id="monitor-config-group-entities-trigger"
           htmlFor="monitor-config-group-entities-switch"
-        /> : null
+        /> :
+        <span
+          id="monitor-config-group-entities-disabled-trigger"
+          data-tip="Elige al menos 2 entidades, programas o actividades para poder agruparlas"
+        >
+          Agrupar
+          <ReactTooltip className="monitor-config-group-trigger-tooltip"/>
+        </span>
     )
   }
 }
