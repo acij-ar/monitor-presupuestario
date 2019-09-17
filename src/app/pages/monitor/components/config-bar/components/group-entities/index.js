29c8384c03661ad2593014553d9eb4340607ee4d
Ignacio Heredia <ignacio.nh@gmail.com>
2019-09-17 19:35:16 -0300

    Client side unification logic

--- src/app/pages/monitor/components/config-bar/components/group-entities/index.js
+++ src/app/pages/monitor/components/config-bar/components/group-entities/index.js
@@ -9,26 +9,35 @@ class GroupEntities extends React.Component {
     this.state = {
       nonGroupedItems: this.props.selected,
       firstGroupItems: [],
+      firstGroupUnified: false,
       secondGroupItems: [],
+      secondGroupUnified: false,
     };
 
     this.onDragEnd = this.onDragEnd.bind(this);
   }
 
-  static getDerivedStateFromProps(props, state) {
+  static getDerivedStateFromProps(props, oldState) {
     const newState = {
       nonGroupedItems: [],
       firstGroupItems: [],
+      firstGroupUnified: oldState.firstGroupUnified,
       secondGroupItems: [],
+      secondGroupUnified: oldState.secondGroupUnified
     };
-    state.nonGroupedItems.concat(state.firstGroupItems).concat(state.secondGroupItems).map(item => { item.groupId = null });
-    // TODO: reset unification items
+    const itemsOfPreviousState = oldState.nonGroupedItems.concat(oldState.firstGroupItems).concat(oldState.secondGroupItems);
+    itemsOfPreviousState.map(item => {
+      item.groupId = null;
+      item.unified = false;
+    });
     props.selected.map(item => {
-      if (state.firstGroupItems.includes(item)) {
+      if (oldState.firstGroupItems.includes(item)) {
         item.groupId = 1;
+        item.unified = oldState.firstGroupUnified;
         newState.firstGroupItems.push(item);
-      } else if (state.secondGroupItems.includes(item)) {
+      } else if (oldState.secondGroupItems.includes(item)) {
         item.groupId = 2;
+        item.unified = oldState.secondGroupUnified;
         newState.secondGroupItems.push(item);
       } else {
         item.groupId = null;
@@ -39,8 +48,13 @@ class GroupEntities extends React.Component {
   }
 
   unifyGroups(shouldBeUnified, group) {
-    console.log(`unify ${group}? : ${shouldBeUnified}`);
-    // TODO: imapct items
+    if (group === 'firstGroupItems') {
+      this.state.firstGroupItems.map(item => { item.unified = shouldBeUnified });
+      this.state.firstGroupUnified = shouldBeUnified
+    } else {
+      this.state.secondGroupItems.map(item => { item.unified = shouldBeUnified });
+      this.state.secondGroupUnified = shouldBeUnified
+    }
     this.props.onGroupsChange();
     this.setState(this.state);
   }
