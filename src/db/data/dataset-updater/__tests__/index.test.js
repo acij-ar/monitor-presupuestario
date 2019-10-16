a4eb707ffe792b6a40b36ccf5f016bbdefa576b5
Ignacio Heredia <ignacio.nh@gmail.com>
2019-10-16 18:32:51 -0300

    Add dataset updater script

---
+++ src/db/data/dataset-updater/__tests__/index.test.js
@@ -0,0 +1,37 @@
+const mockCompareAllFiles = jest.fn()
+  .mockReturnValueOnce([{upToDate: false, filename: 'filename.csv', id: 'abc123'}])
+  .mockReturnValueOnce([{upToDate: true}]);
+jest.mock('../compare-all-files', () => mockCompareAllFiles);
+
+const mockDownloadFile = jest.fn();
+const mockInit = jest.fn();
+jest.mock('../../google-drive-client', () => (
+  class MockGoogleDrive {
+    init() { mockInit() }
+    downloadFile(...args) { return mockDownloadFile(...args) }
+  }
+));
+jest.mock('../../../../utils/logger', () => ({ info: jest.fn() }));
+
+const datasetUpdater = require('..');
+
+describe('Dataset updater', () => {
+  afterEach(() => {
+    mockDownloadFile.mockClear();
+    mockInit.mockClear();
+  });
+
+  it('should download outdated datasets', async (done) => {
+    await datasetUpdater();
+    expect(mockInit).toHaveBeenCalledTimes(1);
+    expect(mockDownloadFile).toHaveBeenCalledTimes(1);
+    done();
+  });
+
+  it('should do nothing when all datasets are updated', async (done) => {
+    await datasetUpdater();
+    expect(mockInit).toHaveBeenCalledTimes(0);
+    expect(mockDownloadFile).toHaveBeenCalledTimes(0);
+    done();
+  });
+});
---
+++ src/db/data/dataset-updater/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const compareAllFiles = require('./compare-all-files');
+const GoogleDriveClient = require('../google-drive-client');
+const logger = require('../../../utils/logger');
+
+module.exports = async () => {
+  const fileStatus = await compareAllFiles();
+  const outdatedFiles = fileStatus.filter(file => !file.upToDate);
+
+  if (outdatedFiles.length === 0) {
+    logger.info('All files are up to date');
+    return
+  }
+
+  logger.info(`Outdated files: ${outdatedFiles.length}`);
+  const driveClient = new GoogleDriveClient();
+  await driveClient.init();
+  const downloadPromises = outdatedFiles.map(file => {
+    const outputPath = path.join(__dirname, '..', 'datasets', file.filename);
+    return driveClient.downloadFile({ fileId: file.id, outputPath })
+  });
+  await Promise.all(downloadPromises);
+  logger.info('All files updated');
+};
