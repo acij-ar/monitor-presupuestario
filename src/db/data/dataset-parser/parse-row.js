01a2181bba56417e0b75648aa689319d46605c94
Ignacio Heredia <ignacio.nh@gmail.com>
2019-10-21 18:43:25 -0300

    Small rewrite

--- src/db/data/dataset-parser/parse-row.js
+++ src/db/data/dataset-parser/parse-row.js
@@ -13,10 +13,12 @@ const parseNumericValue = require('./parse-numeric-value');
  * @returns {object} - Row with parsed columns
  */
 module.exports = (row) => {
-  row.credito_presupuestado = parseNumericValue(row.credito_presupuestado);
-  row.credito_vigente = parseNumericValue(row.credito_vigente);
-  row.credito_comprometido = parseNumericValue(row.credito_comprometido);
-  row.credito_devengado = parseNumericValue(row.credito_devengado);
-  row.credito_pagado = parseNumericValue(row.credito_pagado);
-  return row;
+  return {
+    ...row,
+    credito_presupuestado: parseNumericValue(row.credito_presupuestado),
+    credito_vigente: parseNumericValue(row.credito_vigente),
+    credito_comprometido: parseNumericValue(row.credito_comprometido),
+    credito_devengado: parseNumericValue(row.credito_devengado),
+    credito_pagado: parseNumericValue(row.credito_pagado),
+  };
 };
