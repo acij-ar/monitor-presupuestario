7633dfb8c602e8977bf280b86e2e211f1908d0a9
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-03 22:27:44 -0300

    Change base object

--- src/db/data/dataset-parser/__tests__/base-json-object.test.js
+++ src/db/data/dataset-parser/__tests__/base-json-object.test.js
@@ -11,6 +11,21 @@ describe('Base json object', () => {
       credito_devengado: 0,
       credito_pagado: 0,
       credito_original: 0,
+      rows: [],
+    };
+    expect(baseObject).toEqual(expectedObject);
+  });
+
+  it('should match schema for leaf objects', () => {
+    const baseObject = baseJSONObject(false);
+    const expectedObject = {
+      credito_presupuestado: 0,
+      credito_vigente: 0,
+      credito_comprometido: 0,
+      credito_devengado: 0,
+      credito_pagado: 0,
+      credito_original: 0,
+      rows: [],
     };
     expect(baseObject).toEqual(expectedObject);
   })
--- src/db/data/dataset-parser/base-json-object.js
+++ src/db/data/dataset-parser/base-json-object.js
@@ -1,14 +1,16 @@
 /**
  * Returns the base object used for the json file of each year
  *
+ * @param {boolean} canHaveChildDependencies - Wheter or not the dependency can have sub dependencies
  * @returns {object} - Base object for json file of each year
  */
-module.exports = () => ({
-  dependencias: {},
+module.exports = (canHaveChildDependencies = true) => ({
+  dependencias: canHaveChildDependencies ? {} : undefined,
   credito_presupuestado: 0,
   credito_vigente: 0,
   credito_comprometido: 0,
   credito_devengado: 0,
   credito_pagado: 0,
   credito_original: 0,
+  rows: [],
 });
