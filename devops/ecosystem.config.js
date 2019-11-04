d6d546ce9636ae2f319e25ae6e79438f376b17a7
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-03 21:03:44 -0300

    Absolute paths in pm2 config file

--- devops/ecosystem.config.js
+++ devops/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps : [{
     name: 'server',
-    script: '~/repositories/acij-ar/monitor-presupuestario/dist/server.js',
+    script: '/home/iheredia/repositories/acij-ar/monitor-presupuestario/dist/server.js',
     interpreter: 'node',
   }, {
     name: 'server',
-    script: '~/repositories/acij-ar/test-monitor-presupuestario/dist/server.js',
+    script: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario/dist/server.js',
     env: {
       PORT: "8081",
     },
