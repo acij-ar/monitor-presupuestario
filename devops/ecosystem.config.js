89e4f53a96256f884a4f907500d08df210b6f244
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-03 21:19:28 -0300

    Fix relative paths in pm2 config file

--- devops/ecosystem.config.js
+++ devops/ecosystem.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   apps : [{
     name: 'server',
-    cwd: '/home/iheredia/repositories/acij-ar/monitor-presupuestario',
-    script: '/home/iheredia/repositories/acij-ar/monitor-presupuestario/dist/server.js',
-    interpreter: 'node',
+    cwd: '/home/iheredia/repositories/acij-ar/monitor-presupuestario/',
+    script: './dist/server.js',
   }, {
     name: 'test-server',
-    cwd: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario',
-    script: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario/dist/server.js',
-    interpreter: 'node',
+    cwd: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario/',
+    script: './dist/server.js',
     env: {
       PORT: "8081",
     },
