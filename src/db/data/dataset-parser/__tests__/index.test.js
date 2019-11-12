6ca44c13d1ac9f720851c80426d3760b0dd15c6a
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-12 19:22:10 -0300

    Reading inflation file in the first step of the parsing script

--- src/db/data/dataset-parser/__tests__/index.test.js
+++ src/db/data/dataset-parser/__tests__/index.test.js
@@ -4,17 +4,24 @@ jest.mock('../../files', () => [
   { id: 1, year: 2019 },
   { id: 2, year: 2019 },
   { id: 3, year: 2020 },
-  { id: 4, inflation: true },
+  { id: 4, inflation: true, path: 'path-to-inflation.csv' },
 ]);
+const mockReadCSV = jest.fn(({onData}) => {
+  onData({ ejercicio_presupuestario: '2019', tasa_ajuste_inflacion: '1.2' });
+  onData({ ejercicio_presupuestario: '2020', tasa_ajuste_inflacion: '1' });
+});
+jest.mock('../../../../utils/read-csv', () => mockReadCSV);
 
 const datasetParser = require('..');
 
 describe('Dataset parser', () => {
   it('should iterate through each file and call datasetToJSON on them', async (done) => {
     await datasetParser();
+    expect(mockReadCSV).toHaveBeenCalledTimes(1);
+    expect(mockReadCSV.mock.calls[0][0].path).toBe('path-to-inflation.csv');
     expect(mockDatasetToJSON).toHaveBeenCalledTimes(2);
-    expect(mockDatasetToJSON.mock.calls[0][0]).toEqual({ files: [{id: 1, year: 2019}, { id: 2, year: 2019 }], inflationFile: { id: 4, inflation: true } });
-    expect(mockDatasetToJSON.mock.calls[1][0]).toEqual({ files: [{id: 3, year: 2020}], inflationFile: { id: 4, inflation: true } });
+    expect(mockDatasetToJSON.mock.calls[0][0]).toEqual({ files: [{id: 1, year: 2019}, { id: 2, year: 2019 }], inflation: 1.2 });
+    expect(mockDatasetToJSON.mock.calls[1][0]).toEqual({ files: [{id: 3, year: 2020}], inflation: 1 });
     done();
   });
 });
--- src/db/data/dataset-parser/index.js
+++ src/db/data/dataset-parser/index.js
@@ -1,6 +1,7 @@
 const datasetToJSON = require('./dataset-to-json');
 const files = require('../files');
 const logger = require('../../../utils/logger');
+const readCSV = require('../../../utils/read-csv');
 
 const inflationFile = files.find(file => file.inflation);
 const years = [];
@@ -12,10 +13,20 @@ files.map(file => file.year && years.indexOf(file.year) === -1 ? years.push(file
  * @returns {Promise<void>} - Promise that resolves when all files are done processing
  */
 module.exports = async () => {
-  logger.info('Processing csv files into db.json');
+  logger.info('Processing csv files into json files');
+  const inflationByYear = {};
+  await readCSV({
+    path: inflationFile.path,
+    onData: (row) => {
+      const year = parseInt(row.ejercicio_presupuestario);
+      inflationByYear[year] = parseFloat(row.tasa_ajuste_inflacion);
+    }
+  });
   for (let i=0; i<years.length; i++) {
     const year = years[i];
     const yearFiles = files.filter(file => file.year === year);
-    await datasetToJSON({ files: yearFiles, inflationFile })
+    const inflation = inflationByYear[year];
+    await datasetToJSON({ files: yearFiles, inflation });
   }
+  logger.info('Finished processing csv files into json files');
 };
