a0048d5645a1b2796408f0d9b4585c1d2eff3170
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-13 19:01:16 -0300

    Standarizing all rows

--- src/db/data/dataset-parser/__tests__/parse-row.test.js
+++ src/db/data/dataset-parser/__tests__/parse-row.test.js
@@ -1,23 +1,147 @@
-const mockParseNumericValue = jest.fn(() => 'parsed-numeric-value');
+const mockParseNumericValue = jest.fn((string, numberUnitsAreMillions) => {
+  if (string === '') {
+    return 0
+  } else if (numberUnitsAreMillions) {
+    return parseFloat(string.replace(',', '.')) * 1e6
+  }
+  return parseFloat(string)
+});
 jest.mock('../parse-numeric-value', () => mockParseNumericValue);
 
 const parseRow = require('../parse-row');
 
 describe('Parse row', () => {
-  it('should call parse-numeric-value on each numeric column of the rows', () => {
+  it('should handle rows from hacienda', () => {
     const mockRow = {
-      credito_presupuestado: '123',
+      jurisdiccion_desc: 'nombre jurisdiccion',
+      entidad_desc: 'entidad nombre',
+      programa_desc: 'programa',
+      actividad_desc: 'actividad_',
+      credito_presupuestado: '123,123',
       credito_vigente: '234',
-      credito_comprometido: '345',
+      credito_comprometido: '345,444',
       credito_devengado: '456',
       credito_pagado: '567',
     };
-    const newRow = parseRow(mockRow);
-    expect(newRow.credito_presupuestado).toBe('parsed-numeric-value');
-    expect(newRow.credito_vigente).toBe('parsed-numeric-value');
-    expect(newRow.credito_comprometido).toBe('parsed-numeric-value');
-    expect(newRow.credito_devengado).toBe('parsed-numeric-value');
-    expect(newRow.credito_pagado).toBe('parsed-numeric-value');
-    expect(mockParseNumericValue.mock.calls).toEqual([['123'], ['234'], ['345'], ['456'], ['567']]);
+
+    const newRow = parseRow({
+      row: mockRow,
+      numberUnitsAreMillions: true,
+      usePresupuestadoAsOriginal: true,
+      inflation: 2,
+      filename: 'filename.csv',
+      rowIndex: 123,
+    });
+    expect(newRow).toEqual({
+      raw: {
+        jurisdiccion_desc: 'nombre jurisdiccion',
+        entidad_desc: 'entidad nombre',
+        programa_desc: 'programa',
+        actividad_desc: 'actividad_',
+        credito_presupuestado: '123,123',
+        credito_vigente: '234',
+        credito_comprometido: '345,444',
+        credito_devengado: '456',
+        credito_pagado: '567',
+      },
+      parsed: {
+        adjusted: {
+          aumento: 0,
+          comprometido: 690888000,
+          devengado: 912000000,
+          original: 246246000,
+          originalConAumento: 246246000,
+          pagado: 1134000000,
+          presupuestado: 246246000,
+          vigente: 468000000,
+        },
+        marcarHijosConPosibleReasignacion: false,
+        origin:  {
+          filename: 'filename.csv',
+          inflation: 2,
+          rowIndex: 123,
+        },
+        pen:  {
+          actividad: 'actividad_',
+          entidad: 'entidad nombre',
+          jurisdiccion: 'nombre jurisdiccion',
+          programa: 'programa',
+        },
+        unadjusted:  {
+          aumento: 0,
+          comprometido: 345444000,
+          devengado: 456000000,
+          original: 123123000,
+          originalConAumento: 123123000,
+          pagado: 567000000,
+          presupuestado: 123123000,
+          vigente: 234000000,
+        },
+      },
+    });
+  });
+
+  it('should handle rows from presupuesto-original', () => {
+    const mockRow = {
+      jurisdiccion: 'nombre jurisdiccion',
+      entidad: 'entidad nombre',
+      programa: 'programa',
+      actividad: 'actividad_',
+      credito_original: '123',
+      aumento: '345'
+    };
+
+    const newRow = parseRow({
+      row: mockRow,
+      numberUnitsAreMillions: false,
+      usePresupuestadoAsOriginal: false,
+      inflation: 2,
+      filename: 'filename.csv',
+      rowIndex: 123,
+    });
+    expect(newRow).toEqual({
+      raw: {
+        jurisdiccion: 'nombre jurisdiccion',
+        entidad: 'entidad nombre',
+        programa: 'programa',
+        actividad: 'actividad_',
+        credito_original: '123',
+        aumento: '345'
+      },
+      parsed: {
+        adjusted: {
+          aumento: 690,
+          comprometido: 0,
+          devengado: 0,
+          original: 246,
+          originalConAumento: 936,
+          pagado: 0,
+          presupuestado: 0,
+          vigente: 0,
+        },
+        marcarHijosConPosibleReasignacion: false,
+        origin:  {
+          filename: 'filename.csv',
+          inflation: 2,
+          rowIndex: 123,
+        },
+        pen:  {
+          actividad: 'actividad_',
+          entidad: 'entidad nombre',
+          jurisdiccion: 'nombre jurisdiccion',
+          programa: 'programa',
+        },
+        unadjusted:  {
+          aumento: 345,
+          comprometido: 0,
+          devengado: 0,
+          original: 123,
+          originalConAumento: 468,
+          pagado: 0,
+          presupuestado: 0,
+          vigente: 0,
+        },
+      },
+    });
   });
 });
--- src/db/data/dataset-parser/parse-row.js
+++ src/db/data/dataset-parser/parse-row.js
@@ -1,24 +1,69 @@
 const parseNumericValue = require('./parse-numeric-value');
 
 /**
- * Converts columns with string representations of numbers into
- * their numeric equivalent values
+ * Parse rows. Returns an object with both the raw data and the parsed data.
+ * All objects returned from this method have the same structure and attrs.
  *
- * @param {object} row - Row with columns to be parsed
- * @param {string} row.credito_presupuestado - String representation of number
- * @param {string} row.credito_vigente - String representation of number
- * @param {string} row.credito_comprometido - String representation of number
- * @param {string} row.credito_devengado - String representation of number
- * @param {string} row.credito_pagado - String representation of number
- * @returns {object} - Row with parsed columns
+ * @param {object} params - Row with columns to be parsed
+ * @param {object} params.row - Row with columns to be parsed
+ * @param {boolean} params.numberUnitsAreMillions - Flag that signals if numeric represent millions
+ * @param {boolean} params.usePresupuestadoAsOriginal - Flag that signals if credito_presupuestado should be treated as credito_original
+ * @param {number} params.inflation - Inflation for the year
+ * @param {string} params.filename - Filename that identifies the origin of the row being parsed
+ * @param {number} params.rowIndex - Index of the row in the original file
+ * @returns {object} - Object with parsed row
  */
-module.exports = (row) => {
+module.exports = ({row, numberUnitsAreMillions, usePresupuestadoAsOriginal, inflation, filename, rowIndex}) => {
+  const parse = (string) => parseNumericValue(string || '', numberUnitsAreMillions);
+
+  const original = parse(usePresupuestadoAsOriginal ? row.credito_presupuestado : row.credito_original);
+  const presupuestado = parse(row.credito_presupuestado);
+  const vigente = parse(row.credito_vigente);
+  const comprometido = parse(row.credito_comprometido);
+  const devengado = parse(row.credito_devengado);
+  const pagado = parse(row.credito_pagado);
+  const aumento = parse(row.aumento);
+  const marcarHijosConPosibleReasignacion = (row.marcar_hijos_con_posible_reasignacion || '').trim().toLowerCase() === 'si';
+  const jurisdiccion = row.jurisdiccion || row.jurisdiccion_desc;
+  const entidad = row.entidad || row.entidad_desc;
+  const programa = row.programa || row.programa_desc;
+  const actividad = row.actividad || row.actividad_desc;
+
   return {
-    ...row,
-    credito_presupuestado: parseNumericValue(row.credito_presupuestado),
-    credito_vigente: parseNumericValue(row.credito_vigente),
-    credito_comprometido: parseNumericValue(row.credito_comprometido),
-    credito_devengado: parseNumericValue(row.credito_devengado),
-    credito_pagado: parseNumericValue(row.credito_pagado),
+    raw: row,
+    parsed: {
+      marcarHijosConPosibleReasignacion,
+      pen: {
+        jurisdiccion,
+        entidad,
+        programa,
+        actividad,
+      },
+      origin: {
+        filename,
+        rowIndex,
+        inflation,
+      },
+      unadjusted: {
+        original,
+        originalConAumento: original + aumento,
+        presupuestado,
+        vigente,
+        comprometido,
+        devengado,
+        pagado,
+        aumento,
+      },
+      adjusted: {
+        original: original * inflation,
+        originalConAumento: (original + aumento) * inflation,
+        presupuestado: presupuestado * inflation,
+        vigente: vigente * inflation,
+        comprometido: comprometido * inflation,
+        devengado: devengado * inflation,
+        pagado: pagado * inflation,
+        aumento: aumento * inflation,
+      },
+    },
   };
 };
