2e21cafdf03179974bab03db1cdfb386f39c256e
Ignacio Heredia <ignacio.nh@gmail.com>
2019-11-13 19:05:18 -0300

    Add tests for parseNumericValues new updates

--- src/db/data/dataset-parser/__tests__/parse-numeric-value.test.js
+++ src/db/data/dataset-parser/__tests__/parse-numeric-value.test.js
@@ -1,21 +1,36 @@
 const parseNumericValue = require('../parse-numeric-value');
 
 describe('Parse numeric value', () => {
-  it('should handle simple ints', () => {
-    expect(parseNumericValue('0')).toBe(0);
-    expect(parseNumericValue('1')).toBe(1e6);
-    expect(parseNumericValue('2')).toBe(2e6);
-    expect(parseNumericValue('10')).toBe(1e7);
-    expect(parseNumericValue('50')).toBe(5e7);
-    expect(parseNumericValue('100')).toBe(1e8);
-    expect(parseNumericValue('1000')).toBe(1e9);
+  it('should handle simple ints when units are millions', () => {
+    expect(parseNumericValue('0', true)).toBe(0);
+    expect(parseNumericValue('1', true)).toBe(1e6);
+    expect(parseNumericValue('2', true)).toBe(2e6);
+    expect(parseNumericValue('10', true)).toBe(1e7);
+    expect(parseNumericValue('50', true)).toBe(5e7);
+    expect(parseNumericValue('100', true)).toBe(1e8);
+    expect(parseNumericValue('1000', true)).toBe(1e9);
   });
 
-  it('should handle decimal values', () => {
-    expect(parseNumericValue('0,123456')).toBe(123456);
-    expect(parseNumericValue('1,000001')).toBe(1000001);
-    expect(parseNumericValue('0,000001')).toBe(1);
-    expect(parseNumericValue('0,0000001')).toBe(0.1);
-    expect(parseNumericValue('1,00000001')).toBe(1000000.01);
+  it('should handle decimal values when units are millions', () => {
+    expect(parseNumericValue('0,123456', true)).toBe(123456);
+    expect(parseNumericValue('1,000001', true)).toBe(1000001);
+    expect(parseNumericValue('0,000001', true)).toBe(1);
+    expect(parseNumericValue('0,0000001', true)).toBe(0.1);
+    expect(parseNumericValue('1,00000001', true)).toBe(1000000.01);
+  });
+
+  it('should handle non numeric strings', () => {
+    expect(parseNumericValue('abc')).toBe(0);
+    expect(parseNumericValue('123abc')).toBe(0);
+    expect(parseNumericValue('abc123')).toBe(0);
+    expect(parseNumericValue('')).toBe(0);
+    expect(parseNumericValue(',^^%')).toBe(0);
+  });
+
+  it('should handle common float strings', () => {
+    expect(parseNumericValue('1')).toBe(1);
+    expect(parseNumericValue('2')).toBe(2);
+    expect(parseNumericValue('123.456')).toBe(123.456);
+    expect(parseNumericValue('10.10')).toBe(10.1);
   });
 });
--- src/db/data/dataset-parser/parse-numeric-value.js
+++ src/db/data/dataset-parser/parse-numeric-value.js
@@ -1,15 +1,28 @@
+const numberRegex = /^\s*\d+([,.]\d+)?\s*$/;
+
 /**
- * Parses string representations of numbers where each unit represents a million
+ * Parses string representations of numbers
+ *
+ * When the numberUnitsAreMillions is used, units are treated as representing millions
  * and the decimal separator is ","
- * Examples:
+ * Examples for numberUnitsAreMillions = true:
  * "1" is equivalent to 1000000 (1e6)
  * "0,123456" is equivalent to 123456
  * "0,0000001" is equivalent to 0.1
  *
  * @param {string} numericString - String representing a number
+ * @param {boolean} [numberUnitsAreMillions] - Flag representing if the numberic units represents millions
  * @returns {number} - Parsed value of the input string
  */
-module.exports = (numericString) => {
+module.exports = (numericString, numberUnitsAreMillions) => {
+  if (typeof numericString !== 'string' || !numericString.match(numberRegex)) {
+    return 0;
+  }
+
+  if (!numberUnitsAreMillions) {
+    return parseFloat(numericString)
+  }
+
   if (numericString.includes(',')) {
     const [ integerString, decimalString ] = numericString.split(',');
     let decimalValue;
