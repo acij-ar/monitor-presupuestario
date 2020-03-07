d5128173a9915ae8c60afe692bf0f616ee6bc9ca
Ignacio Heredia <ignacio.nh@gmail.com>
2020-03-07 19:27:34 -0300

    Connect the new components for the upcoming dataset updater

--- src/app/pages/admin/view.js
+++ src/app/pages/admin/view.js
@@ -2,17 +2,22 @@ const React = require('react');
 const Page = require('../../components/page');
 const TextsForm = require('./components/texts-form');
 const DatasetForm = require('./components/datasets-form');
+const PropTypes = require('prop-types');
 
-const App = ({ texts, datasets }) => (
+const App = ({ texts }) => (
   <Page>
     <div className="monitor-highlight">
       <div className="monitor-content">
         <h1>Admin</h1>
       </div>
     </div>
-    <DatasetForm datasets={datasets}/>
+    <DatasetForm />
     <TextsForm texts={texts}/>
   </Page>
 );
+
+App.propTypes = {
+  texts: PropTypes.object.isRequired,
+};
 
 module.exports = App;
