a637adc5d8c705c5cd1b4337aad906a479b7291d
Ignacio Heredia <ignacio.nh@gmail.com>
2020-03-11 17:39:36 -0300

    Update pm2 config file

--- devops/ecosystem.config.js
+++ devops/ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps : [{
     name: 'server',
     cwd: '/home/iheredia/repositories/acij-ar/monitor-presupuestario/',
-    script: './dist/server.js',
+    script: './src/server.js',
     env: {
       MONIT_LOG_NAME: 'SERVER',
     },
   }, {
     name: 'test-server',
     cwd: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario/',
-    script: './dist/server.js',
+    script: './src/server.js',
     env: {
       PORT: "8081",
       MONIT_LOG_NAME: 'TEST-SERVER',
