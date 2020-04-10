3a4e420dbf6c5b982002c5eb50e3ad5bd49e46ff
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-10 12:22:29 -0300

    Update pm2 and nginx example files

--- devops/ecosystem.config.js
+++ devops/ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps : [{
     name: 'server',
-    cwd: '/home/iheredia/repositories/acij-ar/monitor-presupuestario/',
+    cwd: '/home/monitor/repositories/acij/monitor-presupuestario/',
     script: './src/server.js',
     env: {
       MONIT_LOG_NAME: 'SERVER',
     },
   }, {
     name: 'test-server',
-    cwd: '/home/iheredia/repositories/acij-ar/test-monitor-presupuestario/',
+    cwd: '/home/monitor/repositories/acij/test-monitor-presupuestario/',
     script: './src/server.js',
     env: {
       PORT: "8081",
