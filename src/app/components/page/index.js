7d8a4ef48639bad4a372630fbb72e74c6f2a3ae4
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-12 22:09:01 -0300

    Delete old header

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,6 +1,5 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const Header = require('./components/header');
 const Footer = require('./components/footer');
 const Analytics = require('../analytics');
 
@@ -13,7 +12,6 @@ class Page extends React.Component {
     const {children} = this.props;
     return (
       <React.Fragment>
-        <Header key="header"/>
         <div id="main" key="main">
           {children}
         </div>
