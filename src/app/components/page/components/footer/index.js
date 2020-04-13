8c72c09725663625be0cc33eebd2a82ff7c5be25
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-12 22:09:24 -0300

    Refactor footer

--- src/app/components/page/components/footer/index.js
+++ src/app/components/page/components/footer/index.js
@@ -3,19 +3,19 @@ const React = require('react');
 const Footer = () => (
   <div id="footer">
     <div id="footer-content">
-      <a href="/">
-        <img id="footer-logo" src="/static/logo.png" alt="Monitor presupuestario" />
+      <span id="acij-initiative">UNA INICIATIVA DE</span>
+      <a href="http://acij.org.ar/" target="_blank" rel="noopener noreferrer" id="acij-link">
+        <img src="/static/logo-acij.png" alt="ACIJ"/>
       </a>
-      <a href="http://acij.org.ar/" target="_blank" rel="noopener noreferrer">
-        <img id="footer-acij" src="/static/logo-acij.png" alt="ACIJ" />
+      <a href="https://twitter.com/ACIJargentina" target="_blank" rel="noopener noreferrer" className="social-link">
+        <img src="/static/twitter.svg" alt="twitter"/>
       </a>
-      <div id="footer-information">
-        <a href="mailto:info@acij.org.ar">info@acij.org.ar</a>
-        <div>Tel (+54 11) 4381-2371</div>
-        <div>Av. de Mayo 1161, 1 piso</div>
-        <div>(C1085ABB) Bs As, Argentina</div>
-        <a href="http://acij.org.ar/">acij.org.ar</a>
-      </div>
+      <a href="https://www.facebook.com/ACIJ.ORG" target="_blank" rel="noopener noreferrer" className="social-link">
+        <img src="/static/facebook.svg" alt="facebook"/>
+      </a>
+      <a href="https://github.com/acij-ar/monitor-presupuestario" target="_blank" rel="noopener noreferrer" className="social-link">
+        <img src="/static/github.svg" alt="github"/>
+      </a>
     </div>
   </div>
 );
