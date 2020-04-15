4eafd0b300249f67b2721dd9d08fc3abc7dc080a
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-15 20:25:48 -0300

    Add structure for the upcoming menu

---
+++ src/app/components/page/components/menu/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const Menu = () => (
+  <div id="menu">
+    Menu
+  </div>
+);
+
+module.exports = Menu;
--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const Footer = require('./components/footer');
+const Menu = require('./components/menu');
 const Analytics = require('../analytics');
 
 class Page extends React.Component {
@@ -9,11 +10,14 @@ class Page extends React.Component {
   }
 
   render() {
-    const {children} = this.props;
+    const {children, pageName} = this.props;
     return (
       <React.Fragment>
         <div id="main" key="main">
-          {children}
+          <Menu selectedPage={pageName} />
+          <div id="page-content">
+            {children}
+          </div>
         </div>
         <Footer key="footer"/>
       </React.Fragment>
@@ -23,6 +27,7 @@ class Page extends React.Component {
 
 Page.propTypes = {
   children: PropTypes.node,
+  pageName: PropTypes.string.isRequired,
 };
 
 module.exports = Page;
