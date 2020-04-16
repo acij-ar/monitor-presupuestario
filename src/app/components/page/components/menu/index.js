1013323af6dd38c214909351e1050f3dabbb050c
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-15 23:30:31 -0300

    Add a simple sticky menu

--- src/app/components/page/components/menu/index.js
+++ src/app/components/page/components/menu/index.js
@@ -1,11 +1,39 @@
 const React = require('react');
+const PropTypes = require('prop-types');
 
-const Menu = () => (
+const Menu = ({ selectedPage }) => (
   <div id="menu">
     <div id="menu-content">
       <a id="logo" href="/" />
+
+      <ul>
+        <li>
+          <a href="/" id="menu-link-budget" className={selectedPage === 'budget' ? 'active' : ''}>
+            El presupuesto
+          </a>
+        </li>
+        <li>
+          <a href="/monitor" id="menu-link-monitor" className={selectedPage === 'monitor' ? 'active' : ''}>
+            Monitor
+          </a>
+        </li>
+        <li>
+          <a href="/acerca-de" id="menu-link-about" className={selectedPage === 'about' ? 'active' : ''}>
+            Acerca de
+          </a>
+        </li>
+        <li>
+          <a href="/" id="menu-link-doubts" className={selectedPage === 'doubts' ? 'active' : ''}>
+            Dudas
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 );
+
+Menu.propTypes = {
+  selectedPage: PropTypes.string.isRequired,
+}
 
 module.exports = Menu;
