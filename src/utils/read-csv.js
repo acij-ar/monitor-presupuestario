a07c3f6127892da2e6816e5d75b251ce4250e7e8
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-16 20:30:47 -0300

    Add missing error handler to promise

--- src/utils/read-csv.js
+++ src/utils/read-csv.js
@@ -2,10 +2,11 @@ const csv = require('fast-csv');
 const fs = require('fs');
 
 module.exports = ({path, onData}) => (
-  new Promise(resolve => {
+  new Promise((resolve, reject) => {
     fs.createReadStream(path)
       .pipe(csv.parse({headers: true}))
       .on('data', onData)
+      .on('error', reject)
       .on('end', resolve);
   })
 );
