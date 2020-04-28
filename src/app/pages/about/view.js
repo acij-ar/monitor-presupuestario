dcf20bedfdcf7b6d71d47e6c6f2122790f27b9f6
Ignacio Heredia <ignacio.nh@gmail.com>
2020-04-27 23:05:29 -0300

    Add more dummy content to About page

--- src/app/pages/about/view.js
+++ src/app/pages/about/view.js
@@ -2,11 +2,17 @@ const React = require('react');
 const Page = require('../../components/page');
 const PropTypes = require('prop-types');
 const AboutMainContent = require('./components/main-content');
+const AboutACIJ = require('./components/acij');
+const AboutMatchbox = require('./components/matchbox');
+const AboutMethodology = require('./components/methodology');
 
-const App = ({ pageName }) => (
+const App = ({pageName}) => (
   <Page pageName={pageName}>
     <div id="about-main-content">
-      <AboutMainContent />
+      <AboutMainContent/>
+      <AboutACIJ/>
+      <AboutMatchbox/>
+      <AboutMethodology/>
     </div>
   </Page>
 );
