d6a1723fa689a552dc9a4f32770c32ff87743f2f
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-25 17:17:35 -0300

    Update main about content

--- src/app/pages/about/components/main-content/index.js
+++ src/app/pages/about/components/main-content/index.js
@@ -2,15 +2,17 @@ const React = require('react');
 const ContentSection = require('../../../../components/content-section');
 
 const AboutMainContent = () => (
-  <ContentSection id="about-main-content" title="Acerca de">
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-      type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-      remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-      Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-      of Lorem Ipsum.
-    </p>
+  <ContentSection id="about-main-content" title="El Monitor Presupuestario">
+    <p>Desde la Asociación Civil por la Igualdad y la Justicia (ACIJ), creamos el Monitor presupuestario con el objetivo
+      de promover el acceso a información presupuestaria de manera sencilla, desagregada y accesible, y así contribuir a
+      mejorar las posibilidades de incidencia e impacto en las acciones tendientes a visibilizar situaciones de
+      discriminación, exclusión y garantía de derechos.</p>
+    <p>El Monitor presupuestario es una plataforma que contiene información de los diferentes organismos, ministerios,
+      secretarías, programas y actividades presupuestarias desde el año 2007, incluyendo el presupuesto proyectado por
+      el Poder Ejecutivo para 2019.</p>
+    <p>La plataforma permite monitorear la asignación presupuestaria de cada año, el presupuesto aprobado por el
+      Congreso y el presupuesto vigente (con las modificaciones del Poder Ejecutivo), así como la ejecución de años
+      anteriores.</p>
   </ContentSection>
 );
 
