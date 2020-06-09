02f154aee84f2396428d661cf18d708b23dbf998
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-09 20:53:01 -0300

    Add TODOs

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -5,6 +5,8 @@ const requireLogin = require('./controllers/require-login');
 const updateDatasets = require('./controllers/update-datasets');
 const jobStatus = require('./controllers/job-status');
 
+// TODO: remove dataset endpoints
+// TODO: add text management endpoints
 router.post('/update_datasets', requireLogin, updateDatasets);
 router.get('/dataset_job_status', requireLogin, jobStatus);
 router.post('/login', loginController);
--- src/api/db/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// TODO: new data endpoints
-
-module.exports = router;
