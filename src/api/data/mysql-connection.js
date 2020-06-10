719805166332abd614d779713e88fbc8379187ac
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-09 22:28:15 -0300

    Modularize api data controllers

---
+++ src/api/data/mysql-connection.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+const {mysqlconfig: {mysqlconfig}} = require('../../config');
+
+module.exports = () => {
+  let db_connection = mysql.createConnection(mysqlconfig);
+  db_connection.connect();
+  return db_connection
+}
