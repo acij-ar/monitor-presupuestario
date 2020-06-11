1d9df25d0952160c19eb6ae13a0239de9e709272
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-11 16:56:11 -0300

    Starting monitor-explore selector

--- src/app/pages/explore/components/monitor-explorer/components/selector/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/selector/index.js
@@ -1,8 +1,18 @@
 const React = require('react');
 
 const Selector = () => (
-  <div>
-    Selector
+  <div id="monitor-explore-selector">
+    <div id="monitor-explore-selector-labels">
+      <div id="monitor-explore-selector-labels-who">
+        <span>¿Quién?</span>
+      </div>
+      <div id="monitor-explore-selector-labels-reason">
+        <span>¿Para qué?</span>
+      </div>
+      <div id="monitor-explore-selector-labels-what">
+        <span>¿En qué se gasta?</span>
+      </div>
+    </div>
   </div>
 );
 
