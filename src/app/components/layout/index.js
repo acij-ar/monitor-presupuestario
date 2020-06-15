768f460573d36f6fdfe5f9b83ba57a759b799b3b
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-15 18:57:55 -0300

    Using local font files

--- src/app/components/layout/index.js
+++ src/app/components/layout/index.js
@@ -11,7 +11,6 @@ const Layout = ({ children, scripts, styles, componentProps }) => (
     <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
 
     { styles.map(href => <link key={href} rel="stylesheet" href={`/static/${href}`} />) }
-    <link href="https://fonts.googleapis.com/css?family=Rubik:400,500,700,900&display=swap" rel="stylesheet" />
   </head>
   <body>
     <div id="root">{ children }</div>
