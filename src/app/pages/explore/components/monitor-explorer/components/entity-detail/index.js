4a00908a89c1c56eaf28d35940d84644e99c2e1e
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-16 19:37:40 -0300

    Add entity detail component

--- src/app/pages/explore/components/monitor-explorer/components/entity-detail/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/entity-detail/index.js
@@ -1,8 +1,34 @@
 const React = require('react');
 
 const EntityDetail = () => (
-  <div>
-    EntityDetail
+  <div id="monitor-explorer-entity-detail">
+    <div id="monitor-explorer-entity-detail-jurisdiction-name">
+      Nombre jurisdicción
+    </div>
+    <div id="monitor-explorer-entity-detail-jurisdiction-value">
+      % del presupuesto total
+    </div>
+
+    <div id="monitor-explorer-entity-detail-entity-name">
+      &gt; Nombre entidad
+    </div>
+    <div id="monitor-explorer-entity-detail-entity-value">
+      % del presupuesto total <span>J</span>
+    </div>
+
+    <div id="monitor-explorer-entity-detail-program-name">
+      &gt; Nombre programa
+    </div>
+    <div id="monitor-explorer-entity-detail-program-value">
+      % del presupuesto total <span>E</span>
+    </div>
+
+    <div id="monitor-explorer-entity-detail-activity-name">
+      &gt; Nombre actividad
+    </div>
+    <div id="monitor-explorer-entity-detail-activity-value">
+      % del presupuesto total <span>P</span>
+    </div>
   </div>
 );
 
