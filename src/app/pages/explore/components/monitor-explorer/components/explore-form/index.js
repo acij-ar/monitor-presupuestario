bed8561fa8d789fcc21d672a40c6a04e7b8b3417
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-17 21:11:15 -0300

    Showing options in explore selects

--- src/app/pages/explore/components/monitor-explorer/components/explore-form/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/explore-form/index.js
@@ -5,30 +5,82 @@ const axios = require('axios');
 const { useEffect, useState } = React;
 
 const ExploreForm = () => {
-  let [options, setOptions] = useState({});
-  useEffect(async () => {
+  const [options, setOptions] = useState({});
+  const fetchData = async () => {
     const { data } = await axios.get('/api/data/options');
     setOptions(data);
-  }, []);
+  };
+  useEffect(() => { fetchData() }, []);
+
+  const [selected, setSelected] = useState({});
+  const updateSelected = newValues => setSelected({ ...selected, ...newValues });
 
   return (
     <div id="monitor-explore-selector">
       <div id="monitor-explore-selector-labels-who">
         <h3>¿Quién?</h3>
-        <Selector id="entity-form-jurisdiccion" name="Jurisdicción" options={options.jurisdictions}/>
-        <Selector id="entity-form-entidad" name="Entidad" options={options.entities}/>
+        <Selector
+          id="entity-form-jurisdiccion"
+          name="Jurisdicción"
+          options={options.jurisdictions}
+          onChange={e => updateSelected({ jurisdiction: e.target.value})}
+          value={selected.jurisdiction}
+        />
+        <Selector
+          id="entity-form-entidad"
+          name="Entidad"
+          options={options.entities}
+          onChange={e => updateSelected({ entity: e.target.value})}
+          value={selected.entity}
+        />
       </div>
       <div id="monitor-explore-selector-labels-reason">
         <h3>¿Para qué?</h3>
-        <Selector id="entity-form-programa" name="Programa" options={options.programs}/>
-        <Selector id="setting-form-years" name="Años disponibles" options={options.years} />
+        <Selector
+          id="entity-form-programa"
+          name="Programa"
+          options={options.programs}
+          onChange={e => updateSelected({ program: e.target.value})}
+          value={selected.program}
+        />
+        <Selector
+          id="setting-form-years"
+          name="Años disponibles"
+          options={options.years}
+          onChange={e => updateSelected({ year: e.target.value})}
+          value={selected.year}
+        />
       </div>
       <div id="monitor-explore-selector-labels-what">
         <h3>¿En qué se gasta?</h3>
-        <Selector id="entity-form-actividad" name="Actividad" options={options.activities}/>
-        <Selector id="entity-form-funcion" name="Función" options={options.functions}/>
-        <Selector id="setting-form-budgets" name="Tipos de presup." options={options.budgets} />
-        <Selector id="setting-form-inlfation" name="Ajus. por inflación" options={options.inflation} />
+        <Selector
+          id="entity-form-actividad"
+          name="Actividad"
+          options={options.activities}
+          onChange={e => updateSelected({ activity: e.target.value})}
+          value={selected.activity}
+        />
+        <Selector
+          id="entity-form-funcion"
+          name="Función"
+          options={options.functions}
+          onChange={e => updateSelected({ function: e.target.value})}
+          value={selected.function}
+        />
+        <Selector
+          id="setting-form-budgets"
+          name="Tipos de presup."
+          options={options.budgets}
+          onChange={e => updateSelected({ budget: e.target.value})}
+          value={selected.budget}
+        />
+        <Selector
+          id="setting-form-inlfation"
+          name="Ajus. por inflación"
+          options={options.inflation}
+          onChange={e => updateSelected({ inflation: e.target.value})}
+          value={selected.inflation}
+        />
       </div>
     </div>
   )
--- src/app/pages/explore/components/monitor-explorer/components/explore-form/selector/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/explore-form/selector/index.js
@@ -1,10 +1,10 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const Selector = ({ id, name, options }) => (
+const Selector = ({ id, name, options, value, onChange }) => (
   <div className="selector-wrapper" id={id}>
-    <select id={`${id}-select`}>
-      <option disabled selected>{name}</option>
+    <select id={`${id}-select`} value={value || 'undefined'} onChange={onChange}>
+      <option disabled value="undefined">{name}</option>
       {
         options && options.map(({ name: optionName, value }) => (
           <option key={optionName} value={value}>{optionName}</option>
@@ -18,8 +18,16 @@ Selector.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  })).isRequired,
+    name: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]).isRequired,
+  })),
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  onChange: PropTypes.func.isRequired,
 };
 
 module.exports = Selector;
