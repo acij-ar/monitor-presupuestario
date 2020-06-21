5e74408a976982e6f407fa1319d7154f9f110d5a
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-20 22:36:10 -0300

    Move selected logic to monitor-explorer

--- src/app/pages/explore/components/monitor-explorer/components/explore-form/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/explore-form/index.js
@@ -1,8 +1,6 @@
 const React = require('react');
+const PropTypes = require('prop-types');
 const Selector = require('./selector');
-const axios = require('axios');
-
-const { useEffect, useState } = React;
 
 const getMostRecentYear = (options) => {
   if (!options || options.length === 0) return null;
@@ -19,86 +17,98 @@ const getDefaultInflation = (options) => {
   return 'Ajustado'
 }
 
-const ExploreForm = () => {
-  const [options, setOptions] = useState({});
-  const fetchData = async () => {
-    const { data } = await axios.get('/api/data/options');
-    setOptions(data);
-  };
-  useEffect(() => { fetchData() }, []);
-
-  const [selected, setSelected] = useState({});
-  const updateSelected = newValues => setSelected({ ...selected, ...newValues });
-
-  return (
-    <div id="monitor-explore-selector">
-      <div id="monitor-explore-selector-labels-who">
-        <h3>¿Quién?</h3>
-        <Selector
-          id="entity-form-jurisdiccion"
-          name="Jurisdicción"
-          options={options.jurisdictions}
-          onChange={e => updateSelected({ jurisdiction: e.target.value})}
-          value={selected.jurisdiction}
-        />
-        <Selector
-          id="entity-form-entidad"
-          name="Entidad"
-          options={options.entities}
-          onChange={e => updateSelected({ entity: e.target.value})}
-          value={selected.entity}
-        />
-      </div>
-      <div id="monitor-explore-selector-labels-reason">
-        <h3>¿Para qué?</h3>
-        <Selector
-          id="entity-form-programa"
-          name="Programa"
-          options={options.programs}
-          onChange={e => updateSelected({ program: e.target.value})}
-          value={selected.program}
-        />
-        <Selector
-          id="setting-form-years"
-          name="Años disponibles"
-          options={options.years}
-          onChange={e => updateSelected({ year: e.target.value})}
-          value={selected.year || getMostRecentYear(options.years)}
-        />
-      </div>
-      <div id="monitor-explore-selector-labels-what">
-        <h3>¿En qué se gasta?</h3>
-        <Selector
-          id="entity-form-actividad"
-          name="Actividad"
-          options={options.activities}
-          onChange={e => updateSelected({ activity: e.target.value})}
-          value={selected.activity}
-        />
-        <Selector
-          id="entity-form-funcion"
-          name="Función"
-          options={options.functions}
-          onChange={e => updateSelected({ function: e.target.value})}
-          value={selected.function}
-        />
-        <Selector
-          id="setting-form-budgets"
-          name="Tipos de presup."
-          options={options.budgets}
-          onChange={e => updateSelected({ budget: e.target.value})}
-          value={selected.budget || getDefaultBudget(options.budgets)}
-        />
-        <Selector
-          id="setting-form-inlfation"
-          name="Ajuste por inflación"
-          options={options.inflation}
-          onChange={e => updateSelected({ inflation: e.target.value})}
-          value={selected.inflation || getDefaultInflation(options.inflation)}
-        />
-      </div>
+const ExploreForm = ({ options, updateSelected, selected }) => (
+  <div id="monitor-explore-selector">
+    <div id="monitor-explore-selector-labels-who">
+      <h3>¿Quién?</h3>
+      <Selector
+        id="entity-form-jurisdiccion"
+        name="Jurisdicción"
+        options={options.jurisdictions}
+        onChange={e => updateSelected({ jurisdiction: e.target.value})}
+        value={selected.jurisdiction}
+      />
+      <Selector
+        id="entity-form-entidad"
+        name="Entidad"
+        options={options.entities}
+        onChange={e => updateSelected({ entity: e.target.value})}
+        value={selected.entity}
+      />
     </div>
-  )
+    <div id="monitor-explore-selector-labels-reason">
+      <h3>¿Para qué?</h3>
+      <Selector
+        id="entity-form-programa"
+        name="Programa"
+        options={options.programs}
+        onChange={e => updateSelected({ program: e.target.value})}
+        value={selected.program}
+      />
+      <Selector
+        id="setting-form-years"
+        name="Años disponibles"
+        options={options.years}
+        onChange={e => updateSelected({ year: e.target.value})}
+        value={selected.year || getMostRecentYear(options.years)}
+      />
+    </div>
+    <div id="monitor-explore-selector-labels-what">
+      <h3>¿En qué se gasta?</h3>
+      <Selector
+        id="entity-form-actividad"
+        name="Actividad"
+        options={options.activities}
+        onChange={e => updateSelected({ activity: e.target.value})}
+        value={selected.activity}
+      />
+      <Selector
+        id="entity-form-funcion"
+        name="Función"
+        options={options.functions}
+        onChange={e => updateSelected({ function: e.target.value})}
+        value={selected.function}
+      />
+      <Selector
+        id="setting-form-budgets"
+        name="Tipos de presup."
+        options={options.budgets}
+        onChange={e => updateSelected({ budget: e.target.value})}
+        value={selected.budget || getDefaultBudget(options.budgets)}
+      />
+      <Selector
+        id="setting-form-inlfation"
+        name="Ajuste por inflación"
+        options={options.inflation}
+        onChange={e => updateSelected({ inflation: e.target.value})}
+        value={selected.inflation || getDefaultInflation(options.inflation)}
+      />
+    </div>
+  </div>
+);
+
+ExploreForm.propTypes = {
+  updateSelected: PropTypes.func.isRequired,
+  selected: PropTypes.shape({
+    jurisdiction: PropTypes.string,
+    entity: PropTypes.string,
+    program: PropTypes.string,
+    activity: PropTypes.string,
+    year: PropTypes.string,
+    function: PropTypes.string,
+    budget: PropTypes.string,
+    inflation: PropTypes.string,
+  }),
+  options: PropTypes.shape({
+    jurisdictions: PropTypes.array,
+    entities: PropTypes.array,
+    programs: PropTypes.array,
+    activities: PropTypes.array,
+    years: PropTypes.array,
+    functions: PropTypes.array,
+    budgets: PropTypes.array,
+    inflation: PropTypes.array,
+  }),
 };
 
 module.exports = ExploreForm;
--- src/app/pages/explore/components/monitor-explorer/index.js
+++ src/app/pages/explore/components/monitor-explorer/index.js
@@ -5,20 +5,35 @@ const EntityDetail = require('./components/entity-detail');
 const EntityHierarchy = require('./components/entity-hierarchy');
 const EntityTable = require('./components/entity-table');
 const EntityTimeseries = require('./components/entity-timeseries');
+const axios = require('axios');
 
-const MonitorExplorer = () => (
-  <div id="monitor-explorer">
-    <ExploreForm />
-    <div className="monitor-explorer-chart-row">
-      <EntityDetail />
-      <EntitySunburst />
+const { useEffect, useState } = React;
+
+const MonitorExplorer = () => {
+  const [options, setOptions] = useState({});
+  const fetchData = async () => {
+    const { data } = await axios.get('/api/data/options');
+    setOptions(data);
+  };
+  useEffect(() => { fetchData() }, []);
+
+  const [selected, setSelected] = useState({});
+  const updateSelected = newValues => setSelected({ ...selected, ...newValues });
+
+  return (
+    <div id="monitor-explorer">
+      <ExploreForm options={options} updateSelected={updateSelected} selected={selected} />
+      <div className="monitor-explorer-chart-row">
+        <EntityDetail selected={selected} />
+        <EntitySunburst selected={selected} />
+      </div>
+      <div className="monitor-explorer-chart-row">
+        <EntityHierarchy />
+        <EntityTable />
+      </div>
+      <EntityTimeseries />
     </div>
-    <div className="monitor-explorer-chart-row">
-      <EntityHierarchy />
-      <EntityTable />
-    </div>
-    <EntityTimeseries />
-  </div>
-);
+  );
+};
 
 module.exports = MonitorExplorer;
