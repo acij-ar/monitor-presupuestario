87de88f511b04063c1d08f1916fd23a7151c164f
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-23 19:56:31 -0300

    Add goals component for explore page

--- src/app/pages/explore/components/monitor-explorer/components/entity-goals/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/entity-goals/index.js
@@ -1,9 +1,45 @@
 const React = require('react');
+const PropTypes = require('prop-types');
+const axios = require('axios');
 
-const EntityGoals = () => (
-  <div>
-    EntityGoals
-  </div>
-);
+const { useEffect, useState } = React;
+
+const EntityGoals = ({ params }) => {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/data/goals', { params })
+      .then(({ data }) => setGoals(data));
+  }, [params]);
+
+  return goals && goals.length > 0 ? (
+    <div id="monitor-explorer-entity-goals">
+      <div id="monitor-explorer-entity-goals-reference">
+        ¿Cómo se controla la evolución de este programa / actividad?
+      </div>
+      <div id="monitor-explorer-entity-goals-list">
+        <div id="monitor-explorer-entity-goals-title">
+          Metas físicas
+        </div>
+        <ul>
+          { goals.map(goal => <li key={goal}>{goal}</li>)}
+        </ul>
+      </div>
+    </div>
+  ) : null;
+};
+
+EntityGoals.propTypes = {
+  params: PropTypes.shape({
+    jurisdiction: PropTypes.string,
+    entity: PropTypes.string,
+    program: PropTypes.string,
+    activity: PropTypes.string,
+    year: PropTypes.number,
+    function: PropTypes.string,
+    budget: PropTypes.string,
+    inflation: PropTypes.string,
+  }),
+}
 
 module.exports = EntityGoals;
