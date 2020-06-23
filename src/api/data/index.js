8ae4b75bf500baeb85ae5e095846421b21794114
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-22 21:40:23 -0300

    Add mocked timeseries endpoint

--- src/api/data/index.js
+++ src/api/data/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const sunburstDataController = require('./sunburst');
 const detailDataController = require('./detail');
 const optionsController = require('./options');
+const timeseriesDataController = require('./timeseries');
 
 const router = express.Router();
 
 router.get('/sunburst', sunburstDataController);
 router.get('/detail', detailDataController);
+router.get('/timeseries', timeseriesDataController);
 router.get('/options', optionsController);
 
 module.exports = router;
