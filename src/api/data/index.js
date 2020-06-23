5024e94990e3e50c37dcfa642c56870e371b56cc
Ignacio Heredia <ignacio.nh@gmail.com>
2020-06-22 21:52:41 -0300

    Add mocked hierarchy data endpoint

--- src/api/data/index.js
+++ src/api/data/index.js
@@ -3,12 +3,14 @@ const sunburstDataController = require('./sunburst');
 const detailDataController = require('./detail');
 const optionsController = require('./options');
 const timeseriesDataController = require('./timeseries');
+const hierarchyDataController = require('./hierarchy');
 
 const router = express.Router();
 
 router.get('/sunburst', sunburstDataController);
 router.get('/detail', detailDataController);
 router.get('/timeseries', timeseriesDataController);
+router.get('/hierarchy', hierarchyDataController);
 router.get('/options', optionsController);
 
 module.exports = router;
