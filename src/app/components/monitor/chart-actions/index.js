512b41ebf0b95c4b90903f0ac07c8c1437ab8677
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-02 02:32:39 -0300

    Add download actions for png, svg and jpg images

--- src/app/components/monitor/chart-actions/index.js
+++ src/app/components/monitor/chart-actions/index.js
@@ -1,13 +1,34 @@
 const React = require('react');
 const PropTypes = require('prop-types');
+const domtoimage = require('dom-to-image');
+
+const { useRef } = React;
 
 const ChartActions = ({ children }) => {
+  const chartContainer = useRef(null);
+
+  const downloadImage = (dataUrl, filename) => {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = dataUrl;
+    link.click();
+  };
+
   const onChange = (e) => {
-    console.log(e.target.value);
+    const { value } = e.target;
+    const chartNode = chartContainer.current;
+    const imageOptions = { bgcolor: '#fafafa' };
+    if (value === 'jpg') {
+      domtoimage.toJpeg(chartNode, imageOptions).then(dataUrl => downloadImage(dataUrl, 'chart.jpg'))
+    } else if (value === 'png') {
+      domtoimage.toPng(chartNode, imageOptions).then(dataUrl => downloadImage(dataUrl, 'chart.png'))
+    } else if (value === 'svg') {
+      domtoimage.toSvg(chartNode, imageOptions).then(dataUrl => downloadImage(dataUrl, 'chart.svg'))
+    }
   }
   return (
     <div className="chart-with-actions">
-      <div className="chart-container">
+      <div className="chart-container" ref={chartContainer}>
         {children}
       </div>
       <div className="chart-actions">
@@ -16,10 +37,14 @@ const ChartActions = ({ children }) => {
             <option value="undefined">Descargar cómo</option>
             <option value="png">Descargar imagen (PNG)</option>
             <option value="jpg">Descargar imagen (JPG)</option>
+            {/*
             <option value="pdf">Descargar imagen (PDF)</option>
+            */}
             <option value="svg">Descargar imagen (SVG)</option>
-            <option value="csv">Descargar datos (csv)</option>
+            {/*
+            <option value="csv">Descargar datos (CSV)</option>
             <option value="xls">Descargar datos (XLS)</option>
+            */}
           </select>
         </div>
       </div>
