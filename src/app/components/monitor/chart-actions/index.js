f7f6b153bb65b7f51be261593f00177cfe7ccf99
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-02 02:21:40 -0300

    Export selector styles

--- src/app/components/monitor/chart-actions/index.js
+++ src/app/components/monitor/chart-actions/index.js
@@ -2,17 +2,26 @@ const React = require('react');
 const PropTypes = require('prop-types');
 
 const ChartActions = ({ children }) => {
+  const onChange = (e) => {
+    console.log(e.target.value);
+  }
   return (
     <div className="chart-with-actions">
       <div className="chart-container">
         {children}
       </div>
       <div className="chart-actions">
-        <select>
-          <option>Exportar jpeg</option>
-          <option>Exportar png</option>
-          <option>Exportar svg</option>
-        </select>
+        <div className="chart-actions-export-wrapper">
+          <select value="undefined" onChange={onChange}>
+            <option value="undefined">Descargar cómo</option>
+            <option value="png">Descargar imagen (PNG)</option>
+            <option value="jpg">Descargar imagen (JPG)</option>
+            <option value="pdf">Descargar imagen (PDF)</option>
+            <option value="svg">Descargar imagen (SVG)</option>
+            <option value="csv">Descargar datos (csv)</option>
+            <option value="xls">Descargar datos (XLS)</option>
+          </select>
+        </div>
       </div>
     </div>
   )
