8093e479614318165462cac4c50ddbc063bea615
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-06 22:43:59 -0300

    Add data download for table component

--- src/app/pages/explore/components/monitor-explorer/components/entity-table/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/entity-table/index.js
@@ -5,6 +5,16 @@ const ChartActions = require('../../../../../../components/monitor/chart-actions
 
 const { useEffect, useState } = React;
 
+const generateDataForSheet = (tableData) => {
+  const header = ['Categoria', 'C. Original', 'C. Vigente', 'C. Devengado'];
+  const rows = tableData.map((row) => ({
+    'Categoria': row.name,
+    'C. Original': row.original,
+    'C. Vigente': row.vigente,
+    'C. Devengado': row.devengado,
+  }));
+  return [rows, {header}]
+}
 
 const EntityTable = ({ params }) => {
   const [table, setTable] = useState({});
@@ -23,7 +33,7 @@ const EntityTable = ({ params }) => {
   }, [params]);
 
   return (
-    <ChartActions visible={actionVisible}>
+    <ChartActions visible={actionVisible} generateDataForSheet={() => generateDataForSheet(table)}>
       <div id="monitor-explorer-entity-table">
         {
           table && table.length > 0 ? <table>
