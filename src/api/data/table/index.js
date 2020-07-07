b17af719d588f0195772352dbb3c7991a1682af9
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-06 23:01:41 -0300

    Fix table query

--- src/api/data/table/index.js
+++ src/api/data/table/index.js
@@ -3,7 +3,7 @@ const transformRows = require('./transform-rows');
 
 module.exports = async (req, res, next) => {
   try {
-    const rows = await genericQuery({ ... req.query, budget: 'all' });
+    const rows = await genericQuery({ ... req.query, activity: null, budget: 'all' });
     const response = transformRows(rows, req.query);
     res.json(response);
   } catch (e) {
