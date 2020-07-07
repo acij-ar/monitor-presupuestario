08a3d5adcbd29ff8e4f6a792001855ba45f05e06
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-07 20:13:16 -0300

    Opacity fix for disabled selectors

--- src/app/components/monitor/form/selectors/selector/index.js
+++ src/app/components/monitor/form/selectors/selector/index.js
@@ -1,18 +1,21 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const Selector = ({ id, name, options, value, onChange }) => (
-  <div className="selector-wrapper" id={id}>
-    <select id={`${id}-select`} value={value || 'undefined'} onChange={onChange} disabled={!options || options.length === 0}>
-      <option disabled value="undefined">{name}</option>
-      {
-        options && options.map(({ name: optionName, value, id: optionId }) => (
-          <option key={optionName} value={optionId || value}>{optionName}</option>
-        ))
-      }
-    </select>
-  </div>
-);
+const Selector = ({ id, name, options, value, onChange }) => {
+  const disabled = !options || options.length === 0;
+  return (
+    <div className={`selector-wrapper ${disabled ? 'selector-wrapper-disabled' : ''}`} id={id}>
+      <select id={`${id}-select`} value={value || 'undefined'} onChange={onChange} disabled={disabled}>
+        <option disabled value="undefined">{name}</option>
+        {
+          options && options.map(({ name: optionName, value, id: optionId }) => (
+            <option key={optionName} value={optionId || value}>{optionName}</option>
+          ))
+        }
+      </select>
+    </div>
+  )
+};
 
 Selector.propTypes = {
   id: PropTypes.string.isRequired,
