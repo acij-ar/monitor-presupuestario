b223f7ef586f819dfb5c391b8c1859e95b6baaf7
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-09 16:42:06 -0300

    Cleanup timeseries and nightingale highcharts options

--- src/api/data/timeseries-compare/highcharts-options.js
+++ src/api/data/timeseries-compare/highcharts-options.js
@@ -5,14 +5,19 @@ module.exports = (categories, data) => merge({}, baseHighchartsOptions, {
   chart: {
     type: 'areaspline'
   },
-  legend: {
-    layout: 'vertical',
-    align: 'left',
-    verticalAlign: 'top',
-    x: 150,
-    y: 100,
-    floating: true,
-    borderWidth: 1
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Presupuesto (en millones de pesos)',
+    }
+  },
+  tooltip: {
+    shared: true,
+  },
+  plotOptions: {
+    areaspline: {
+      fillOpacity: 0.5
+    }
   },
   xAxis: {
     categories: [
@@ -24,28 +29,6 @@ module.exports = (categories, data) => merge({}, baseHighchartsOptions, {
       'Saturday',
       'Sunday'
     ],
-    plotBands: [{ // visualize the weekend
-      from: 4.5,
-      to: 6.5,
-      color: 'rgba(68, 170, 213, .2)'
-    }]
-  },
-  yAxis: {
-    title: {
-      text: 'Fruit units'
-    }
-  },
-  tooltip: {
-    shared: true,
-    valueSuffix: ' units'
-  },
-  credits: {
-    enabled: false
-  },
-  plotOptions: {
-    areaspline: {
-      fillOpacity: 0.5
-    }
   },
   series: [{
     name: 'John',
