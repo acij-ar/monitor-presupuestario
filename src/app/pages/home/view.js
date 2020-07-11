bc56b673eb61f5023f842aa4716fbc0fc674458f
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-11 17:08:14 -0300

    Add welcome text to home page

--- src/app/pages/home/view.js
+++ src/app/pages/home/view.js
@@ -2,17 +2,23 @@ const React = require('react');
 const Page = require('../../components/page');
 const PropTypes = require('prop-types');
 
-const App = ({ pageName }) => (
+const App = ({pageName}) => (
   <Page pageName={pageName}>
     <a href="/monitor" id="home-link">
-      <img id="background-768" src="/static/portada-768.jpg" alt="Monitor presupuestario" />
-      <div id="background-1920" />
+      <img id="background-768" src="/static/portada-768.jpg" alt="Monitor presupuestario"/>
+      <div id="background-1920"/>
     </a>
+    <h1>Monitor presupuestario</h1>
+    <p>
+      Te damos la bienvenida al Monitor Presupuestario, una plataforma donde podrás conocer y explorar los datos sobre
+      el presupuesto nacional desde el año 2007 para realizar tu propio monitoreo sobre cómo se distribuyen los recursos
+      del Estado en la Argentina.
+    </p>
   </Page>
 );
 
 App.propTypes = {
   pageName: PropTypes.string.isRequired,
-}
+};
 
 module.exports = App;
