c57abadc11d2f85cb7e2fcb3e2bfef9cda4ff530
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-11 17:38:04 -0300

    Update about texts

--- src/app/pages/about/components/main-content/index.js
+++ src/app/pages/about/components/main-content/index.js
@@ -5,13 +5,12 @@ const AboutMainContent = () => (
   <ContentSection id="about-main-content" title="El Monitor Presupuestario">
     <p>Desde la Asociación Civil por la Igualdad y la Justicia (ACIJ), creamos el Monitor presupuestario con el objetivo
       de promover el acceso a información presupuestaria de manera sencilla, desagregada y accesible, y así contribuir a
-      mejorar las posibilidades de incidencia e impacto en las acciones tendientes a visibilizar situaciones de
-      discriminación, exclusión y garantía de derechos.</p>
+      mejorar las posibilidades de análisis, incidencia e impacto en las acciones tendientes a visibilizar situaciones
+      de discriminación y exclusión en la garantía de derechos.</p>
     <p>El Monitor presupuestario es una plataforma que contiene información de los diferentes organismos, ministerios,
-      secretarías, programas y actividades presupuestarias desde el año 2007, incluyendo el presupuesto proyectado por
-      el Poder Ejecutivo para 2019.</p>
-    <p>La plataforma permite monitorear la asignación presupuestaria de cada año, el presupuesto aprobado por el
-      Congreso y el presupuesto vigente (con las modificaciones del Poder Ejecutivo), así como la ejecución de años
+      secretarías, programas y actividades presupuestarias desde el año 2007 hasta la actualidad.</p>
+    <p>Permite monitorear la asignación presupuestaria de cada año, el presupuesto aprobado por el Congreso y el
+      presupuesto vigente (con las modificaciones del Poder Ejecutivo), así como la ejecución del año en curso y de años
       anteriores.</p>
   </ContentSection>
 );
--- src/app/pages/about/components/methodology/index.js
+++ src/app/pages/about/components/methodology/index.js
@@ -3,15 +3,21 @@ const ContentSection = require('../../../../components/content-section');
 
 const AboutMethodology = () => (
   <ContentSection id="about-methodology" title="Metodología">
-    <p>Para desarrollar el Monitor presupuestario utilizamos la información provista por el Ministerio de Hacienda,
-      específicamente los dataset de Crédito y Gasto completo (distribución diaria acumulada de los créditos
-      presupuestarios con clasificadores completos) desde el año 2007 al año 2018, disponible en el sitio
-      presupuestoabierto.gob.ar .</p>
-    <p>La información sobre el proyecto de presupuesto 2019 fue solicitada por ACIJ al Ministerio de Hacienda por medio
-      de un pedido de acceso a la información. Asimismo, actualmente se encuentra disponible en el sitio del Ministerio
-      de Hacienda en <a href="https://www.minhacienda.gob.ar/onp/presupuesto_ciudadano/index.html" target="_blank"
-                        rel="noopener noreferrer">este link</a>.
-    </p>
+    <p>Para desarrollar el Monitor presupuestario utilizamos la información provista por el Ministerio de Economía de la
+      Nación, específicamente los dataset de Crédito y Gasto completo (distribución anual acumulada de los créditos
+      presupuestarios con clasificadores completos) desde el año 2007 al año 2020, disponible en el sitio
+      www..presupuestoabierto.gob.ar .</p>
+    <p>En los años 2015 a 2019, el presupuesto original que se muestra, refleja los datos del presupuesto aprobado por
+      el Congreso Nacional. Para alcanzar dichos datos, cuya publicación en datos abiertos no es facilitada por el poder
+      legislativo, hemos revisado manualmente los proyectos de ley de presupuesto cuyos dataset si se publican, para
+      plasmar en ellos las definiciones hechas por las legisladoras y legisladores. Este trabajo se realiza manualmente
+      y esperamos con el tiempo, poder hacerlo en los años anteriores. Si encontrás un error o incongruencia, te
+      agradecemos que nos lo hagas saber para realizar las correcciones del caso.</p>
+    <p>Temporalmente, en los años 2007 a 2014, bajo el rótulo “Presupuesto Original” se muestra el presupuesto
+      distribuido por el Poder Ejecutivo conforme los datos abiertos que sus autoridades publican. Durante el año 2020,
+      el Congreso Argentino no ha sancionado un presupuesto mediante una Ley. Siguiendo las previsiones normativas el
+      Poder Ejecutivo, realizó una prórroga del presupuesto 2019, se consigna como presupuesto original, la primer
+      distribución realizada en dicho proceso.</p>
   </ContentSection>
 );
 
