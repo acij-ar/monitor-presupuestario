f67b668fb56dd1d05c31a6b7991f713d35a9d86c
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-20 20:42:33 -0300

    Add glosary content

--- src/app/pages/glosary/view.js
+++ src/app/pages/glosary/view.js
@@ -1,11 +1,19 @@
 const React = require('react');
 const Page = require('../../components/page');
+const ContentSection = require('../../components/content-section');
 const PropTypes = require('prop-types');
+const Term = require('./components/term');
+const terms = require('./terms');
 
 const App = ({pageName}) => (
   <Page pageName={pageName}>
     <div id="glosary-main-content">
-      Glosario
+      <ContentSection id="glosary-main-content" title="Glosario" />
+      {
+        terms.map(term => (
+          <Term key={term.name} {...term} />
+        ))
+      }
     </div>
   </Page>
 );
