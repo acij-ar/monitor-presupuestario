4e9e4ac5a4076b4231d90235142e86055ad19227
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-29 22:42:21 -0300

    Add compare options endpoint

---
+++ src/api/data/options/compare/entities.js
@@ -0,0 +1,23 @@
+const MYSQLConnection = require('../../helpers/query/mysql-connection');
+
+const getDistinct = async (column) => {
+  const db_connection = MYSQLConnection();
+  const query = `SELECT DISTINCT(${column}) FROM monitor.simplificado;`
+  const [rows] = await db_connection.promise().query(query);
+  return rows;
+}
+
+module.exports = async () => {
+  const [
+    jurisdictions,
+    entities,
+    programs,
+    activities,
+  ] = await Promise.all([
+    getDistinct('jurisdiccion_desc'),
+    getDistinct('entidad_desc'),
+    getDistinct('programa_desc'),
+    getDistinct('actividad_desc'),
+  ]);
+  return { jurisdictions, entities, programs, activities };
+};
