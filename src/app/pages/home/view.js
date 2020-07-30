d8c18a5a0bf79f5bcad48771a4d77ba190ccc432
Ignacio Heredia <ignacio.nh@gmail.com>
2020-07-29 22:09:56 -0300

    Remove home image

--- src/app/pages/home/view.js
+++ src/app/pages/home/view.js
@@ -4,10 +4,6 @@ const PropTypes = require('prop-types');
 
 const App = ({pageName}) => (
   <Page pageName={pageName}>
-    <a href="/monitor" id="home-link">
-      <img id="background-768" src="/static/portada-768.jpg" alt="Monitor presupuestario"/>
-      <div id="background-1920"/>
-    </a>
     <h1>Monitor presupuestario</h1>
     <p>
       Te damos la bienvenida al Monitor Presupuestario, una plataforma donde podrás conocer y explorar los datos sobre
