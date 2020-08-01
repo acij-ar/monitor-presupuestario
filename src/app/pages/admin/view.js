1b2c90545898772da1586eedb2b12e41aefbfa8f
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-01 14:21:05 -0300

    Base skeleton componente for admin list editr

--- src/app/pages/admin/view.js
+++ src/app/pages/admin/view.js
@@ -1,12 +1,15 @@
 const React = require('react');
-const Page = require('../../components/page');
 const PropTypes = require('prop-types');
+const Page = require('../../components/page');
+const AdminList = require('./components/admin-list');
 
 const App = ({ pageName }) => (
   <Page pageName={pageName}>
     <div className="monitor-highlight">
       <div className="monitor-content">
         <h1>Admin</h1>
+        <AdminList type="doubts" title="Glosario" />
+        <AdminList type="glosary" title="Dudas" />
       </div>
     </div>
   </Page>
