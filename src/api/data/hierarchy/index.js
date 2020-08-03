db86ee27c8a96740ed7e8e62714ad3777a160afc
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-02 21:35:28 -0300

    Fix hierarchy chart for the new table format

--- src/api/data/hierarchy/index.js
+++ src/api/data/hierarchy/index.js
@@ -3,7 +3,7 @@ const rows2obj = require('../helpers/rows-to-obj');
 
 module.exports = async (req, res, next) => {
   try {
-    const rows = await genericQuery({ ...req.query, activity: null, budget: null });
+    const rows = await genericQuery({ ...req.query, activity: null });
     const maxDepth = req.query.activity || req.query.program ? 4 :
       req.query.entity ? 3 : req.query.jurisdiction ? 2 : 1
     const response = rows2obj(rows, { withBudgets: false, withIds: true, maxDepth });
