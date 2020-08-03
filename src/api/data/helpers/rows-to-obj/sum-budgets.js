9e5230d5cea16e46c9810f2cc6a7f6526ea13903
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-02 22:01:39 -0300

    Fix sunburst chart for the new table format

--- src/api/data/helpers/rows-to-obj/sum-budgets.js
+++ src/api/data/helpers/rows-to-obj/sum-budgets.js
@@ -1,5 +1,7 @@
+const rowParseInt = require('../row-parse-int');
+
 module.exports = (row, obj) => {
-  const {budget, original, vigente, devengado} = row;
+  const {budget, original, vigente, devengado} = rowParseInt(row);
   if (budget) obj.budget += budget;
   if (original) obj.original += original;
   if (vigente) obj.vigente += vigente;
