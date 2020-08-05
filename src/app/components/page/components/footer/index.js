6def8b04907cc6ad6b7a76bb1c6cf2349d015bb9
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-05 20:13:19 -0300

    Update footer image and styles

--- src/app/components/page/components/footer/index.js
+++ src/app/components/page/components/footer/index.js
@@ -3,15 +3,8 @@ const React = require('react');
 const Footer = () => (
   <footer>
     <div id="footer-content">
-      <span id="acij-initiative">UNA INICIATIVA DE</span>
       <a href="http://acij.org.ar/" target="_blank" rel="noopener noreferrer" id="acij-link">
-        <img src="/static/logo-acij-small.png" alt="ACIJ"/>
-      </a>
-      <a href="https://twitter.com/ACIJargentina" target="_blank" rel="noopener noreferrer" className="social-link">
-        <img src="/static/twitter.svg" alt="twitter"/>
-      </a>
-      <a href="https://www.facebook.com/ACIJ.ORG" target="_blank" rel="noopener noreferrer" className="social-link">
-        <img src="/static/facebook.svg" alt="facebook"/>
+        <img src="/static/logo-blanco.svg" alt="ACIJ"/>
       </a>
       <a href="https://github.com/acij-ar/monitor-presupuestario" target="_blank" rel="noopener noreferrer" className="social-link">
         <img src="/static/github.svg" alt="github"/>
