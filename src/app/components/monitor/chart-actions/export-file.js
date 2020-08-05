9d08bba3c4abc804473174b51418314b0d73f777
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-05 20:13:33 -0300

    Update filename for chart export files

--- src/app/components/monitor/chart-actions/export-file.js
+++ src/app/components/monitor/chart-actions/export-file.js
@@ -2,23 +2,25 @@ const domtoimage = require('dom-to-image');
 const downloadFile = require('./download-file');
 const XLSX = require('xlsx');
 
+const filenameSuffix = 'monitorpresupuestario.acij.org.ar'
+
 const exportSheet = (data, type) => {
   const ws = XLSX.utils.json_to_sheet(...data);
   const wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, ws, "Datos");
-  XLSX.writeFile(wb, `chart.${type}`)
+  XLSX.writeFile(wb, `${filenameSuffix}.${type}`)
 }
 
 module.exports = (value, chartNode, generateDataForSheet) => {
   const imageOptions = { bgcolor: '#fafafa' };
   if (value === 'jpg') {
-    domtoimage.toJpeg(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, 'chart.jpg'))
+    domtoimage.toJpeg(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, `${filenameSuffix}.jpg`))
   } else if (value === 'png') {
-    domtoimage.toPng(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, 'chart.png'))
+    domtoimage.toPng(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, `${filenameSuffix}.png`))
   } else if (value === 'svg') {
-    domtoimage.toSvg(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, 'chart.svg'))
+    domtoimage.toSvg(chartNode, imageOptions).then(dataUrl => downloadFile(dataUrl, `${filenameSuffix}.svg`))
   } else if (value === 'pdf') {
-    const options = { filename: 'chart.pdf' };
+    const options = { filename: `${filenameSuffix}.pdf` };
     require('dom-to-pdf')(chartNode, options);
   } else if (value === 'csv') {
     exportSheet(generateDataForSheet(), 'csv');
