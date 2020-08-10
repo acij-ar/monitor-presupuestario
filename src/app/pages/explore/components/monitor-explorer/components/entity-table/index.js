16ea30404100e1864cd81aa4493c5d5f31a098f1
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-09 22:49:50 -0300

    Add loading overlay to table chart

--- src/app/pages/explore/components/monitor-explorer/components/entity-table/index.js
+++ src/app/pages/explore/components/monitor-explorer/components/entity-table/index.js
@@ -3,6 +3,7 @@ const PropTypes = require('prop-types');
 const DataClient = require('../../../../../../components/data-client');
 const ChartActions = require('../../../../../../components/monitor/chart-actions');
 const generateDataForSheet = require('./generate-data-for-sheet');
+const LoadingOverlay = require('../../../../../../components/loading-overlay');
 
 const { useEffect, useState } = React;
 const dataClient = new DataClient({ url: '/api/data/table' });
@@ -10,55 +11,60 @@ const dataClient = new DataClient({ url: '/api/data/table' });
 const EntityTable = ({ params }) => {
   const [table, setTable] = useState({});
   const [actionVisible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const dataCallback = (data) => {
+    setLoading(false);
     setTable(data);
     setVisible(true)
   };
 
   useEffect(() => {
     if (params && params.year) {
+      setLoading(true);
       dataClient.get(params, dataCallback);
     }
   }, [params]);
 
   return (
-    <ChartActions visible={actionVisible} generateDataForSheet={() => generateDataForSheet(table, params)} imageTypesEnabled={false}>
-      <div id="monitor-explorer-entity-table">
-        {
-          table && table.rows && table.rows.length > 0 ? <table>
-            <thead>
-              <tr>
-                <th><span>{table.header.name}</span></th>
-                <th><span>C. Original</span></th>
-                <th><span>C. Vigente</span></th>
-                <th><span>C. Devengado</span></th>
-              </tr>
-            </thead>
-            <tbody>
-            {
-              table.rows.map((row, index) => (
-                <tr key={index}>
-                  <td><span>{row.name}</span></td>
-                  <td><span>$ {row.original.toLocaleString('es')}</span></td>
-                  <td><span>$ {row.vigente.toLocaleString('es')}</span></td>
-                  <td><span>$ {row.devengado.toLocaleString('es')}</span></td>
+    <LoadingOverlay loading={loading}>
+      <ChartActions visible={actionVisible} generateDataForSheet={() => generateDataForSheet(table, params)} imageTypesEnabled={false}>
+        <div id="monitor-explorer-entity-table">
+          {
+            table && table.rows && table.rows.length > 0 ? <table>
+              <thead>
+                <tr>
+                  <th><span>{table.header.name}</span></th>
+                  <th><span>C. Original</span></th>
+                  <th><span>C. Vigente</span></th>
+                  <th><span>C. Devengado</span></th>
                 </tr>
-              ))
-            }
-            </tbody>
-            <tfoot>
-              <tr>
-                <th><span/></th>
-                <th><span/></th>
-                <th><span/></th>
-                <th><span/></th>
-              </tr>
-            </tfoot>
-          </table> : null
-        }
-      </div>
-    </ChartActions>
+              </thead>
+              <tbody>
+              {
+                table.rows.map((row, index) => (
+                  <tr key={index}>
+                    <td><span>{row.name}</span></td>
+                    <td><span>$ {row.original.toLocaleString('es')}</span></td>
+                    <td><span>$ {row.vigente.toLocaleString('es')}</span></td>
+                    <td><span>$ {row.devengado.toLocaleString('es')}</span></td>
+                  </tr>
+                ))
+              }
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th><span/></th>
+                  <th><span/></th>
+                  <th><span/></th>
+                  <th><span/></th>
+                </tr>
+              </tfoot>
+            </table> : null
+          }
+        </div>
+      </ChartActions>
+    </LoadingOverlay>
   );
 };
 
