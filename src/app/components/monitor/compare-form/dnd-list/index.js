e9720e5c595b09042d91b365038d9dae51037ee7
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-11 09:38:17 -0300

    Using the new format of the compare options endpoint in the compare form

--- src/app/components/monitor/compare-form/dnd-list/index.js
+++ src/app/components/monitor/compare-form/dnd-list/index.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const { Droppable, Draggable } = require('react-beautiful-dnd');
+const { Droppable } = require('react-beautiful-dnd');
+const Item = require('./item');
 
 const DndList = ({ items, id, isDropDisabled, direction, className, cloaseable, onRemoveItem }) => (
   <Droppable droppableId={id} direction={direction} isDropDisabled={isDropDisabled} mode="virtual">
@@ -11,26 +12,15 @@ const DndList = ({ items, id, isDropDisabled, direction, className, cloaseable, 
         {...provided.droppableProps}
       >
         {items.map((item, index) => (
-          <Draggable
-            key={item.name}
-            draggableId={`${id}-${item.name}`}
-            index={index}>
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                className={`${className}-item ${snapshot.isDragging ? `${className}-item-dragging` : ''}`}
-                style={provided.draggableProps.style}>
-                {item.name}
-                { cloaseable && (
-                  <svg height="14" width="14" viewBox="0 0 20 20" aria-hidden="true" focusable="false" onClick={() => onRemoveItem({ destination: item.source.droppableId, droppableId: id, index })}>
-                    <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" />
-                  </svg>
-                )}
-              </div>
-            )}
-          </Draggable>
+          <Item
+            key={item.actividad_desc || item.programa_desc || item.entidad_desc || item.jurisdiccion_desc}
+            item={item}
+            index={index}
+            id={id}
+            className={className}
+            onRemoveItem={onRemoveItem}
+            closeable={cloaseable}
+          />
         ))}
         {provided.placeholder}
       </div>
--- src/app/components/monitor/compare-form/helpers/update-params.js
+++ src/app/components/monitor/compare-form/helpers/update-params.js
@@ -1,8 +1,8 @@
 module.exports = (setParams, selectedOptions, selectedGroups) => {
   const years = selectedOptions.years.map(({ value }) => value);
   const groups = [
-    ...selectedGroups[0].map(({ name, source: { droppableId: type } }) => ({ name, type, group: 0 })),
-    ...selectedGroups[1].map(({ name, source: { droppableId: type } }) => ({ name, type, group: 1 })),
+    ...selectedGroups[0].map(item => ({ ...item, source: undefined, group: 0 })),
+    ...selectedGroups[1].map(item => ({ ...item, source: undefined, group: 1 })),
   ];
   setParams({
     ...selectedOptions,
