9b8358b931c97df89a5f040f50451f68169e5068
Ignacio Heredia <ignacio.nh@gmail.com>
2020-08-23 23:57:57 -0300

    Using the new queries in the compare options api

--- src/api/data/options/compare/entities.js
+++ src/api/data/options/compare/entities.js
@@ -1,28 +1,32 @@
 const dbConnection = require('../../helpers/query/mysql-connection');
 
-const getDistinct = async (columns, order) => {
-  const query = `SELECT ${columns} FROM monitor.simplificado GROUP BY ${order} ORDER BY ${order} ASC;`
+const getDistinct = async (columns, table, order) => {
+  const query = `SELECT ${columns} FROM ${table} GROUP BY ${order} ORDER BY ${order} ASC;`
   const [rows] = await dbConnection.query(query);
   return rows;
 }
 
 const getDistinctJurisdictions = () => getDistinct(
   'jurisdiccion_desc',
+  'jurisdiccion_mv',
   'jurisdiccion_desc',
 )
 
 const getDistinctEntities = () => getDistinct(
   'MAX(jurisdiccion_desc) as jurisdiccion_desc, entidad_desc',
+  'entidad_mv',
   'entidad_desc',
 )
 
 const getDistinctPrograms = () => getDistinct(
   'MAX(jurisdiccion_desc) as jurisdiccion_desc, MAX(entidad_desc) as entidad_desc, programa_desc',
+  'programa_mv',
   'programa_desc',
 )
 
 const getDistinctActivities = () => getDistinct(
   'MAX(jurisdiccion_desc) as jurisdiccion_desc, MAX(entidad_desc) as entidad_desc, MAX(programa_desc) as programa_desc, actividad_desc',
+  'actividad_mv',
   'actividad_desc',
 )
 
