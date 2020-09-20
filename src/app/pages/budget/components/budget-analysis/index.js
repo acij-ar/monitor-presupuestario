0eab208d033dc63213472090d5d74b4ffc8f9bf7
Ignacio Heredia <ignacio.nh@gmail.com>
2020-09-19 22:15:51 -0300

    Add guides links to the budget page

--- src/app/pages/budget/components/budget-analysis/index.js
+++ src/app/pages/budget/components/budget-analysis/index.js
@@ -17,6 +17,12 @@ const BudgetAnalysis = () => (
       Comparar del Monitor Presupuestario se pueden a analizar las políticas públicas, programas y actividades
       desarrolladas en las diferentes áreas, estén o no etiquetadas como actividades para evaluar su asignación
       presupuestaria a lo largo de los años, y realizar diferentes comparaciones que se consideren relevantes.</p>
+    <p>Para aprender más sobre el monitoreo presupuestario podés consultar la <a
+      href="https://acij.org.ar/wp-content/uploads/2020/08/GUIA-PRESUPUESTO-GENERO-version-digital-julio-2020.pdf"
+      target="_blank" rel="noreferrer">Guía de Análisis Presupuestario Con Perspectiva De Género</a> o la <a
+      href="https://acij.org.ar/wp-content/uploads/2020/06/Gui%CC%81a-para-el-ana%CC%81lisis-presupuestario-en-violencia-de-ge%CC%81nero-en-Argentina.pdf"
+      target="_blank" rel="noreferrer">Guía Para El Análisis Presupuestario De Políticas Contra La Violencia De Género
+      En Argentina</a> que ACIJ ha escrito como herramientas para las Organizaciones de Sociedad Civil. </p>
   </ContentSection>
 );
 
