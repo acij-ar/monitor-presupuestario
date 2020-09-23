5481ba1f32aca2c21787b92c2569e173620dc748
Ignacio Heredia <ignacio.nh@gmail.com>
2020-09-22 21:24:40 -0300

    Add explore joyride steps

--- src/app/pages/explore/view.js
+++ src/app/pages/explore/view.js
@@ -2,6 +2,8 @@ const React = require('react');
 const Page = require('../../components/page');
 const PropTypes = require('prop-types');
 const MonitorExplorer = require('./components/monitor-explorer');
+const MonitorJoyride = require('../../components/joyride');
+const steps = require('./joyride-steps');
 
 const App = ({ pageName }) => (
   <Page pageName={pageName} selectedSubSection="explore-menu-link">
@@ -9,6 +11,7 @@ const App = ({ pageName }) => (
       Monitor <span> / explorar</span>
     </h1>
     <MonitorExplorer />
+    <MonitorJoyride steps={steps} />
   </Page>
 );
 
