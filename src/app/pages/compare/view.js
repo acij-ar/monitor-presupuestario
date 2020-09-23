08c7a0c1106da1c757de7ec93fdc121bd002fa63
Ignacio Heredia <ignacio.nh@gmail.com>
2020-09-22 21:30:13 -0300

    Add compare joyride steps

--- src/app/pages/compare/view.js
+++ src/app/pages/compare/view.js
@@ -2,6 +2,8 @@ const React = require('react');
 const Page = require('../../components/page');
 const PropTypes = require('prop-types');
 const MonitorComparator = require('./components/monitor-comparator');
+const MonitorJoyride = require('../../components/joyride');
+const steps = require('./joyride-steps');
 
 const App = ({pageName}) => (
   <Page pageName={pageName} selectedSubSection="compare-menu-link">
@@ -9,6 +11,7 @@ const App = ({pageName}) => (
       Monitor <span> / comparar</span>
     </h1>
     <MonitorComparator />
+    <MonitorJoyride steps={steps} />
   </Page>
 );
 
