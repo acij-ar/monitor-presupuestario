d6046db31b7698597b8dc7485d61fd8eaabe27f9
Ignacio Heredia <ignacio.nh@gmail.com>
2020-10-15 15:54:12 -0300

    Update doubts main text

--- src/app/pages/doubts/components/main-content/index.js
+++ src/app/pages/doubts/components/main-content/index.js
@@ -4,9 +4,8 @@ const ContentSection = require('../../../../components/content-section');
 const DoubtsMainContent = () => (
   <ContentSection id="doubts-main-content" title="Preguntas frecuentes">
     <p>
-      En esta sección vas a encontrar algunas preguntas frecuentes a la hora de emprender la tarea de analizar el
-      presupuesto, así como también una serie de materiales complementarios a esta plataforma, que te facilitarán el
-      trabajo
+      En esta sección vas a encontrar algunas preguntas frecuentes a la hora de analizar el presupuesto y una serie de
+      materiales complementarios para hacer ese análisis.
     </p>
   </ContentSection>
 );
