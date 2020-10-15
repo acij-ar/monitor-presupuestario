c78d439d96c4a7490aaecdf242d8fd61761c96d2
Ignacio Heredia <ignacio.nh@gmail.com>
2020-10-15 15:54:39 -0300

    Sort glossary terms alphabetically and add color to glossary links

--- src/app/pages/glosary/index.js
+++ src/app/pages/glosary/index.js
@@ -2,7 +2,7 @@ const Glosary = require('./view');
 const dbConnection = require('../../../api/data/db/mysql-connection');
 
 module.exports = async (req, res, next) => {
-  const query = 'SELECT palabra as name, significado as definition FROM glosario;';
+  const query = 'SELECT palabra as name, significado as definition FROM glosario ORDER BY name;';
   const [terms] = await dbConnection.query(query);
   res.locals.props = { terms };
   res.locals.pageName = 'glosary';
