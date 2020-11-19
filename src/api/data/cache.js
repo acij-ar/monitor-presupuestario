114ec5fc4d7de0a1a8082511de590b7a0e7d27f4
Ignacio Heredia <ignacio.nh@gmail.com>
2020-11-19 09:33:14 -0300

    Dont use cache for local development

--- src/api/data/cache.js
+++ src/api/data/cache.js
@@ -2,12 +2,16 @@ const FileSystemCache = require('file-system-cache').default;
 const path = require('path');
 const stringify = require('fast-json-stable-stringify');
 
+const { CACHE } = process.env;
+const dontUseCache = CACHE === 'FALSE';
+
 const cacheFolder = path.join(__dirname, '..', '..', '..', 'cache');
 const cache = FileSystemCache({ basePath: cacheFolder });
 
 const getKeyForRequest = (req) => `${req.baseUrl}${req.path}${stringify(req.query)}`
 
 const getCache = async (req, res, next) => {
+  if (dontUseCache) return next();
   let cachedResponse;
   try {
     const key = getKeyForRequest(req);
@@ -26,6 +30,7 @@ const getCache = async (req, res, next) => {
 const saveCache = (req, res) => {
   const { response } = res.locals;
   res.json(response);
+  if (dontUseCache) return;
   const key = getKeyForRequest(req);
   cache.save([{ key, value: response }]);
 };
