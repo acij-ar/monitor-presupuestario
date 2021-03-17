7320ae9a2ec4e2ca708b1f078644729e1a8dd7ab
Ignacio Heredia <ignacio.nh@gmail.com>
2021-03-17 18:42:41 -0300

    Adds export data for compare-timeseries chart

--- src/app/pages/compare/components/monitor-comparator/components/entities-timeseries-area/generate-data-for-sheet.js
+++ src/app/pages/compare/components/monitor-comparator/components/entities-timeseries-area/generate-data-for-sheet.js
@@ -1,6 +1,20 @@
-module.exports = (chartData) => {
-  const header = ['Año'];
+module.exports = (chartData, params) => {
+  const { xAxis: { categories }, series } = chartData;
+
+  const header = ['Año', 'Grupo', 'Presupuesto', 'Tipo de presupuesto', 'Ajuste por inflación'];
   const rows = [];
-  // TODO
+
+  categories.map((year, index) => {
+    series.map((entity) => {
+      rows.push({
+        'Año': year,
+        'Grupo': entity.name,
+        'Presupuesto': entity.data[index],
+        'Tipo de presupuesto': params.budget,
+        'Ajuste por inflación': params.inflation,
+      });
+    })
+  });
+
   return [rows, {header}]
 }
