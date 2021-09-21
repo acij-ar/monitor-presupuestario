2005bdd6ac0d5d6688aa6e0c9a63b692a9386e0b
Ignacio Heredia <ignacio.nh@gmail.com>
2021-09-21 12:43:16 +0200

    Update instructions for ssl cert

--- devops/https_setup.sh
+++ devops/https_setup.sh
@@ -1,12 +1,7 @@
 # Certificate setup instructions
-# https://certbot.eff.org/lets-encrypt/debianjessie-nginx
+# https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
+sudo snap install core; sudo snap refresh core
 sudo apt-get remove certbot
-wget https://dl.eff.org/certbot-auto
-sudo mv certbot-auto /usr/local/bin/certbot-auto
-sudo chown root /usr/local/bin/certbot-auto
-sudo chmod 0755 /usr/local/bin/certbot-auto
-sudo /usr/local/bin/certbot-auto --nginx
-echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/local/bin/certbot-auto renew -q" | sudo tee -a /etc/crontab > /dev/null
-
-# Open 443 port
-sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot --nginx
